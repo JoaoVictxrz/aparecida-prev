7163a9cdc70007c5840e39652240c43dcdebc4c3
João Victor <jvbatistagomes11@gmail.com>
2024-04-01 14:21:31 -0300

    style: menu close svg

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,153 +1,197 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
-import { FaInstagram, FaYoutube } from 'react-icons/fa'
-import { useState } from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { FaInstagram, FaYoutube } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Home() {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
-            <header className="w-full bg-white p-2 px-5 flex items-center justify-between">
-                <Image
-                    src="/logo-aparecidaprev-1.png"
-                    alt="logo"
-                    width={200}
-                    height={200}
-                    className="w-44"
-                />
-                <ProGestao />
-                <div className="lg:hidden">
-                    {isOpen ? (
-                        <button onClick={() => setIsOpen(!isOpen)} className="text-black">
-                            <Image
-                                src="/menu-close.svg"
-                                alt="logo"
-                                width={200}
-                                height={200}
-                                className="w-6 lg:hidden"
-                            />
-                        </button>
-                    ) : (
-                        <button onClick={() => setIsOpen(!isOpen)}>
-                            <Image
-                                src="/menu.svg"
-                                alt="logo"
-                                width={200}
-                                height={200}
-                                className="w-6 lg:hidden"
-                            />
-                        </button>
-                    )}
-                </div>
-            </header>
-            <nav className="duration-500 trabsition-all">
-                {isOpen ? <MobileNavbar /> : <DesktopNavbar />}
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <header className="w-full bg-white p-2 px-5 flex items-center justify-between">
+        <Image
+          src="/logo-aparecidaprev-1.png"
+          alt="logo"
+          width={200}
+          height={200}
+          className="w-44"
+        />
+        <ProGestao />
+        <div className="lg:hidden">
+          {isOpen ? (
+            <button onClick={() => setIsOpen(!isOpen)} className="text-black">
+              <Image
+                src="/menu-close.svg"
+                alt="logo"
+                width={200}
+                height={200}
+                className="w-6 lg:hidden"
+              />
+            </button>
+          ) : (
+            <button onClick={() => setIsOpen(!isOpen)}>
+              <Image
+                src="/menu.svg"
+                alt="logo"
+                width={200}
+                height={200}
+                className="w-6 lg:hidden"
+              />
+            </button>
+          )}
+        </div>
+      </header>
+      <nav className="duration-500 trabsition-all">
+        {isOpen ? (
+          <MobileNavbar isOpen={isOpen} setIsOpen={setIsOpen} />
+        ) : (
+          <DesktopNavbar />
+        )}
+      </nav>
+    </>
+  );
 }
 
-export const MobileNavbar = () => {
-    return (
-        <nav className="w-full bg-white text-black duration-500 trabsition-all">
-            <ul className="lg:hidden text-center flex flex-col items-center justify-center pb-5">
-                <Links href="/" text="HOME" />
-                <Links href="/sobre" text="SOBRE" />
-                <Links href="/institucional" text="INSTITUCIONAL" />
-                <Links href="/beneficio" text="BENEFÍCIO" />
-                <Links href="/conselho" text="CONSELHO" />
-                <Links href="/financeiro" text="FINANCEIRO" />
-                <Links href="/legislacao" text="LEGISLAÇÃO" />
-                <Links href="/ouvidoria" text="OUVIDORIA" />
-                concurso
-                <Links href="/contato" text="CONTATO" />
-                {/* Pro-Gestão */}
-                <div className="flex flex-col items-center justify-center">
-                    <Link
-                        href="/"
-                        className="flex hover:bg-zinc-200 hover:rounded-md p-2"
-                    >
-                        <Image
-                            src="/selo-pro-gestao.png"
-                            alt="logo"
-                            width={200}
-                            height={200}
-                            className="w-10"
-                        />
-                        <span className="text-xl text-zinc-500 font-light hover:text-zinc-700 p-2">
-                            Pró-Gestão RPPS
-                        </span>
-                    </Link>
-                    <div className="flex gap-5">
-                        <Link href="/" className="hover:text-orange-600">
-                            <FaInstagram size={20} />
-                        </Link>
-                        <Link href="/" className="hover:text-red-600">
-                            <FaYoutube size={20} />
-                        </Link>
-                    </div>
-                </div>
-            </ul>
-        </nav>
-    )
-}
+export const MobileNavbar = ({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: any;
+}) => {
+  return (
+    <nav className="w-full bg-white text-black duration-500 trabsition-all">
+      <ul className="lg:hidden text-center flex flex-col items-center justify-center pb-5">
+        <Links onClick={() => setIsOpen(!isOpen)} href="/" text="HOME" />
+        <Links onClick={() => setIsOpen(!isOpen)} href="/sobre" text="SOBRE" />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/institucional"
+          text="INSTITUCIONAL"
+        />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/beneficio"
+          text="BENEFÍCIO"
+        />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/conselho"
+          text="CONSELHO"
+        />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/financeiro"
+          text="FINANCEIRO"
+        />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/legislacao"
+          text="LEGISLAÇÃO"
+        />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/ouvidoria"
+          text="OUVIDORIA"
+        />
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/concurso"
+          text="CONCURSO"
+        />
+
+        <Links
+          onClick={() => setIsOpen(!isOpen)}
+          href="/contato"
+          text="CONTATO"
+        />
+        {/* Pro-Gestão */}
+        <div className="flex flex-col items-center justify-center">
+          <Link
+            href="/"
+            className="flex hover:bg-zinc-200 hover:rounded-md p-2"
+          >
+            <Image
+              src="/selo-pro-gestao.png"
+              alt="logo"
+              width={200}
+              height={200}
+              className="w-10"
+            />
+            <span className="text-xl text-zinc-500 font-light hover:text-zinc-700 p-2">
+              Pró-Gestão RPPS
+            </span>
+          </Link>
+          <div className="flex gap-5">
+            <Link href="/" className="hover:text-orange-600">
+              <FaInstagram size={20} />
+            </Link>
+            <Link href="/" className="hover:text-red-600">
+              <FaYoutube size={20} />
+            </Link>
+          </div>
+        </div>
+      </ul>
+    </nav>
+  );
+};
 
 export const DesktopNavbar = () => {
-    return (
-        <nav className="hidden w-full lg:flex lg:items-center">
-            <ul className=" bg-white flex w-full justify-end px-5">
-                <Links href="/" text="HOME" />
-                <Links href="/sobre" text="SOBRE" />
-                <Links href="/institucional" text="INSTITUCIONAL" />
-                <Links href="/beneficio" text="BENEFÍCIO" />
-                <Links href="/conselho" text="CONSELHO" />
-                <Links href="/financeiro" text="FINANCEIRO" />
-                <Links href="/legislacao" text="LEGISLAÇÃO" />
-                <Links href="/ouvidoria" text="OUVIDORIA" />
-                <Links href="/concurso" text="CONCURSOS" />
-                <Links href="/contato" text="CONTATO" />
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="hidden w-full lg:flex lg:items-center">
+      <ul className=" bg-white flex w-full justify-end px-5">
+        <Links href="/" text="HOME" />
+        <Links href="/sobre" text="SOBRE" />
+        <Links href="/institucional" text="INSTITUCIONAL" />
+        <Links href="/beneficio" text="BENEFÍCIO" />
+        <Links href="/conselho" text="CONSELHO" />
+        <Links href="/financeiro" text="FINANCEIRO" />
+        <Links href="/legislacao" text="LEGISLAÇÃO" />
+        <Links href="/ouvidoria" text="OUVIDORIA" />
+        <Links href="/concurso" text="CONCURSOS" />
+        <Links href="/contato" text="CONTATO" />
+      </ul>
+    </nav>
+  );
+};
 
 export const ProGestao = () => {
-    return (
-        <div className="hidden lg:flex flex-col items-center justify-center">
-            <Link href="/" className="flex hover:bg-zinc-200 hover:rounded-md p-2">
-                <Image
-                    src="/selo-pro-gestao.png"
-                    alt="logo"
-                    width={200}
-                    height={200}
-                    className="w-5"
-                />
-                <span className="text-sm text-zinc-500 font-light hover:text-zinc-700 ">
-                    Pró-Gestão RPPS
-                </span>
-            </Link>
-            <div className="flex gap-5">
-                <Link href="/" className=" text-zinc-500 hover:text-orange-600">
-                    <FaInstagram size={15} />
-                </Link>
-                <Link href="/" className="text-zinc-500 hover:text-red-600">
-                    <FaYoutube size={15} />
-                </Link>
-            </div>
-        </div>
-    )
-}
-
-export const Links = ({ href, text }: any) => {
-    return (
-        <Link
-            href={`${href}`}
-            className="text-xl md:text-sm text-zinc-500 font-light hover:text-zinc-700 p-2"
-        >
-            {text}
+  return (
+    <div className="hidden lg:flex flex-col items-center justify-center">
+      <Link href="/" className="flex hover:bg-zinc-200 hover:rounded-md p-2">
+        <Image
+          src="/selo-pro-gestao.png"
+          alt="logo"
+          width={200}
+          height={200}
+          className="w-5"
+        />
+        <span className="text-sm text-zinc-500 font-light hover:text-zinc-700 ">
+          Pró-Gestão RPPS
+        </span>
+      </Link>
+      <div className="flex gap-5">
+        <Link href="/" className=" text-zinc-500 hover:text-orange-600">
+          <FaInstagram size={15} />
         </Link>
-    )
-}
+        <Link href="/" className="text-zinc-500 hover:text-red-600">
+          <FaYoutube size={15} />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export const Links = ({ href, text, onClick }: any) => {
+  return (
+    <Link
+      href={`${href}`}
+      onClick={onClick}
+      className="text-xl md:text-sm text-zinc-500 font-light hover:text-zinc-700 p-2"
+    >
+      {text}
+    </Link>
+  );
+};
