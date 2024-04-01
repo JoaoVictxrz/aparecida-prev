05a918f8e2388dbed5655817c1cfe98ba867723d
João Victor <jvbatistagomes11@gmail.com>
2024-04-01 16:59:59 -0300

    feat: pagina acordaos tribunal de contas

---
+++ src/app/institucional/acordaos-do-tribunal-de-contas/page.tsx
@@ -0,0 +1,38 @@
+import { Metadata } from "next";
+import Link from "next/link";
+import LinkAzul from "../components/links";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Acórdãos do tribunal de contas",
+  };
+}
+
+export default function Home() {
+  return (
+    <article className="w-full flex justify-center bg-white pb-2">
+      <section className="px-2 container">
+        <h1 className="font-bold uppercase text-lg md:text-2xl my-8 border-b-[1px] pb-2 ">
+          Acórdãos do tribunal de contas
+        </h1>
+        <div className="w-full container flex flex-col gap-5 pb-20">
+          <p className="font-semibold text-xl text-center">
+            Aparecida Prev - Acórdãos do tribunal de contas
+          </p>
+          <LinkAzul
+            text="ACORDÃO Nº 07973 – 2023-2022"
+            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/11/PROCESSO-2023-2022-2.pdf"
+          />
+          <LinkAzul
+            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/ACORDAO-No-00007-2022-2021.pdf"
+            text="ACORDÃO Nº 00007 – 2022-2021"
+          />
+          <LinkAzul
+            text="ACORDÃO Nº 03568 – 2021-2020"
+            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/ACORDAO-No-03568-2021-2020.pdf"
+          />
+        </div>
+      </section>
+    </article>
+  );
+}
