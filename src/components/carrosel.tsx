45e7b163ec60662433cb1fb29dfa81d64faf39ad
João Victor <jvbatistagomes11@gmail.com>
2024-04-01 14:21:03 -0300

    feat: adicionado calendario na pagina principal

--- src/components/carrosel.tsx
+++ src/components/carrosel.tsx
@@ -1,62 +1,44 @@
-import React, { useState } from 'react'
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
-import { RxDotFilled } from 'react-icons/rx'
-const imageTest = 'https://static.pingendo.com/cover-moon.svg'
+import React, { useState } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 export default function Carrosel() {
-  const slides = [
-    {
-      url: imageTest,
-    },
-    {
-      url: imageTest,
-    },
-    {
-      url: imageTest,
-    },
+  const slides = [{}];
 
-    {
-      url: imageTest,
-    },
-    {
-      url: imageTest,
-    },
-  ]
-
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
-    setCurrentIndex(newIndex)
-  }
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1
-    const newIndex = isLastSlide ? 0 : currentIndex + 1
-    setCurrentIndex(newIndex)
-  }
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
 
   const goToSlide = (slideIndex: any) => {
-    setCurrentIndex(slideIndex)
-  }
+    setCurrentIndex(slideIndex);
+  };
 
   return (
     <div className="bg-white w-full">
-      <div className=" max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div className=" max-w-[1400px] h-[900px] w-full m-auto py-16 px-4 relative group">
         <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+          style={{ backgroundImage: `url(${"banner-calendario.jpg"})` }}
           className="w-full h-full bg-center bg-cover duration-500"
         ></div>
-        {/* Left Arrow */}
+        {/* Left Arrow
         <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div>
         {/* Right Arrow */}
-        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        {/* <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        <div className="flex top-4 justify-center py-2">
+        </div> */}
+        {/* <div className="flex top-4 justify-center py-2">
           {slides.map((slide, slideIndex) => (
             <div
               key={slideIndex}
@@ -65,9 +47,9 @@ export default function Carrosel() {
             >
               <RxDotFilled />
             </div>
-          ))}
-        </div>
+          ))} */}
+        {/* </div> */}
       </div>
     </div>
-  )
+  );
 }
