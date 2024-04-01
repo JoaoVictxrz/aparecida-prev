ec6076882386a08d20ed276218bd2667be2edc95
João Victor <jvbatistagomes11@gmail.com>
2024-04-01 16:59:20 -0300

    feat: pagina de audiencia publica

---
+++ src/app/institucional/audiencia-publica/audiencia-publica-2023/page.tsx
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+import Image from "next/image";
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Audiencia publica 2023",
+  };
+}
+
+export default function Home() {
+  return (
+    <article className="w-full flex justify-center bg-white pb-2">
+      <section className="px-2 container flex flex-col items-center pt-10 pb-20">
+        <Image
+          src="/audiencia-publica/Post-Instagram-Comunicado-Amarelo-Corporativo-1.jpg"
+          alt="Comunicado amarelo corporativo"
+          width={1080}
+          height={500}
+          className="w-[800px] h-[800px]"
+        />
+        <div className="lg:w-[800px] lg:h-[800px">
+          <h1 className="text-xl font-bold pt-4">Audiência Pública 2023</h1>
+          <h2 className="font-light text-sm pb-2">
+            Postado em: 14 de julho de 2023
+          </h2>
+          <p className="text-lg">
+            O Presidente do Robes Venancio convida a população do entorno do
+            AparecidaPrev, os servidores ativos e inativos, representantes da
+            sociedade organizada e demais interessado a participarem da
+            Audiência Pública, a ser realizada no auditório do Instituto de
+            Previdência de Aparecida de Goiânia.
+          </p>
+        </div>
+      </section>
+    </article>
+  );
+}
---
+++ src/app/institucional/audiencia-publica/audiencia-publica-de-prestacao-de-contas-2022-e-realizada/page.tsx
@@ -0,0 +1,107 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Audiência Pública de Prestação de Contas 2022 é realizada",
+  };
+}
+
+export default function Home() {
+  return (
+    <article className="w-full flex justify-center bg-white pb-2">
+      <section className="px-2 container flex flex-col items-center pt-10 pb-20">
+        <Image
+          src="/audiencia-publica/images.png"
+          alt="Imagem"
+          width={274}
+          height={184}
+        />
+        <div className="lg:w-[800px] lg:h-[800px">
+          <h1 className="text-xl font-bold pt-4">
+            Audiência Pública de Prestação de Contas 2022 é realizada
+          </h1>
+          <h2 className="font-light text-sm pb-2">
+            Postado em: 25 de julho de 20233
+          </h2>
+          <p className="py-2">
+            No último dia 24/7/2023, no auditório do AparecidaPrev, foi
+            realizada a audiência pública conduzida pelo diretor financeiro,
+            Khayo Eduardo, Alcir Antônio de Azevedo, atuário (Atuar
+            Consultoria), Reiter Ferreira Peixoto (Sete Capital Assessoria) e o
+            presidente do AparecidaPrev, Robes Venancio e Silva, no qual teve
+            como pauta o Relatório de Governança Corporativa, os resultados da
+            Avaliação Atuarial e acompanhamento da Política de Investimento do
+            Fundo Previdenciário do AparecidaPrev exercício 2022.
+          </p>
+          <p className="py-2">
+            Essa ação converge com as ações da administração direta, que tem o
+            apoio do prefeito Vilmar Mariano, vem em medida para trazer mais
+            transparência aos servidores ativos e inativos sobre o que está
+            sendo desempenhado no Instituto, além de “oxigenar” novas ideias
+            para melhorar os serviços entregues aos nossos beneficiários.
+          </p>
+          <p className="py-2">
+            O papel social do AparecidaPrev é conceder e gerir os benefícios
+            previdenciários com qualidade, respeito e responsabilidade, fornecer
+            informações e soluções adequadas, bem como atuar na promoção da
+            melhoria da qualidade de vida dos beneficiários e servidores.
+          </p>
+          <p className="py-2">
+            O Presidente do AparecidaPrev destacou dois pontos importantes:
+          </p>
+          <p className="py-2">
+            “… essa a prestação de contas é um passo importante dentro da
+            gestão, reforçando a conduta da Instituição que tem vistas no
+            desenvolvimento da cultura de compliance, de modo que os
+            interessados possam acompanhar de forma mais clara as informações
+            ...”
+          </p>
+          <p className="py-2">
+            “… é o AparecidaPrev cumprindo seu compromisso público junto aos
+            servidores ativos, aposentados, pensionistas e de modo geral com
+            toda sociedade.”
+          </p>
+          <p className="py-2">
+            Durante a audiência foram apresentadas as ações desenvolvidas no
+            decorrer do ano de 2022 para os beneficiários do AparecidaPrev e
+            também dados referentes: Gestão Previdenciária, Avaliação Atuarial
+            2022, Gestão Financeira e Orçamentária.
+          </p>
+          <p className="py-2">
+            Participaram também da Audiência Pública as diretoras
+            administrativa, jurídica e interina de benefícios, Hellen Cássia,
+            Keila Miriam Martins, Juliana Mendanha, respectivamente e os
+            servidores do instituto.
+          </p>
+          <div className="grid md:grid-cols-2 place-items-center gap-2">
+            <Image
+              src="/audiencia-publica/imagem-1.jpeg"
+              alt=""
+              width={300}
+              height={225}
+            />
+            <Image
+              src="/audiencia-publica/imagem-2.jpeg"
+              alt=""
+              width={300}
+              height={225}
+            />
+            <Image
+              src="/audiencia-publica/imagem-3.jpeg"
+              alt=""
+              width={300}
+              height={225}
+            />
+            <Image
+              src="/audiencia-publica/imagem-4.jpeg"
+              alt=""
+              width={300}
+              height={200}
+            />
+          </div>
+        </div>
+      </section>
+    </article>
+  );
+}
