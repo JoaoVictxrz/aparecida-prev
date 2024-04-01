347726777a40e4037c5f3ed0d9565d54ec3e19de
João Victor <jvbatistagomes11@gmail.com>
2024-04-01 17:02:18 -0300

    feat: pagina de gestão financeira e administrativa

---
+++ src/app/institucional/gestao-financeira-e-administrativa/page.tsx
@@ -0,0 +1,34 @@
+import { Metadata } from "next";
+import LinkAzul from "../components/links";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Gestão financeira e administrativa",
+  };
+}
+
+export default function Home() {
+  return (
+    <article className="w-full flex justify-center bg-white pb-2">
+      <section className="px-2 container">
+        <h1 className="font-bold uppercase text-lg md:text-2xl my-8 border-b-[1px] pb-2 ">
+          Gestão financeira e administrativa
+        </h1>
+        <div className="w-full flex flex-col container gap-5 pb-20">
+          <LinkAzul
+            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Gestao-2017-a-2022.pdf"
+            text="Relatório de Gestão Financeira e Administrativa 2017 a 2022"
+          />
+          <LinkAzul
+            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Gestao-2017-a-2021-Atualizado.pdf"
+            text="Relatório de Gestão Financeira e Administrativa 2017 a 2021"
+          />
+          <LinkAzul
+            href="https://aparecidaprev.go.gov.br//wp-content/uploads/2015/07/gestao.pdf"
+            text="Relatório de Gestão Financeira e Administrativa 2009 a 2015"
+          />
+        </div>
+      </section>
+    </article>
+  );
+}
