c4e1ff3a34b6844f616df198bf6c93974d0236c6
João Victor <jvbatistagomes11@gmail.com>
2024-04-02 16:25:18 -0300

    feat: pagina instituições financeiras e slug com rotas

---
+++ src/app/institucional/instituicoes-financeiras/[slug]/page.tsx
@@ -0,0 +1,170 @@
+import { Metadata } from "next";
+import Image from "next/image";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const title = params.slug;
+  return {
+    title: title,
+  };
+}
+
+export default function Page({ params }: Props) {
+  return <GeneratePage params={params} />;
+}
+
+export const GeneratePage = ({ params }: Props) => {
+  switch (params.slug) {
+    case "banco-genail":
+      return (
+        <PaginaSlug nome="banco genail" postadoEm="19 de março de 2024">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2024/03/download-300x147.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "banco-master":
+      return (
+        <PaginaSlug nome="banco master" postadoEm="27 de fevereiro de 2024">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2024/02/Banco-Master-300x76.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "btg-pactual":
+      return (
+        <PaginaSlug nome="banco pactual" postadoEm="2 de outubro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-BTG.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "planner":
+      return (
+        <PaginaSlug nome="planner" postadoEm="2 de outubro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-planner.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "banco-santander":
+      return (
+        <PaginaSlug nome="banco santander" postadoEm="2 de outubro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-sANTANDER.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "xp-investimentos":
+      return (
+        <PaginaSlug nome="xp investimentos" postadoEm="2 de outubro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/INVESTIMENTOS.jpg"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "banco-bradesco":
+      return (
+        <PaginaSlug nome="banco bradesco" postadoEm="2 de outubro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-BRADESCO.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "banco-safra":
+      return (
+        <PaginaSlug nome="banco safra" postadoEm="2 de outubro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-safra.png"
+            alt=""
+            width={300}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "banco-itau":
+      return (
+        <PaginaSlug nome="banco itaú" postadoEm="27 de setembro de 2023">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/09/Banco-Itau.png"
+            alt=""
+            width={200}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "banco-do-brasil":
+      return (
+        <PaginaSlug nome="banco do brasil" postadoEm="17 de fevereiro de 2014">
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2014/02/Banco-Brasil.png"
+            alt=""
+            width={200}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+    case "caixa-economica-federal":
+      return (
+        <PaginaSlug
+          nome="caixa economica federal"
+          postadoEm="2 de janeiro de 2014"
+        >
+          <Image
+            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2014/01/Caixa.png"
+            alt=""
+            width={200}
+            height={147}
+          />
+        </PaginaSlug>
+      );
+  }
+};
+
+interface props {
+  nome: string;
+  postadoEm: string;
+  children?: React.ReactNode;
+}
+
+export const PaginaSlug = ({ nome, postadoEm, children }: props) => {
+  return (
+    <div className="w-full flex justify-center gap-5 pb-20 bg-white dark:bg-zinc-800 dark:text-zinc-100">
+      <div className="container flex-col">
+        <p className="text-xl md:text-2xl uppercase pt-10 font-semibold">
+          {nome}
+        </p>
+        <p className="text-lg md:text-base font-extralight">
+          Postado em: {postadoEm}
+        </p>
+        <div className="pt-4 p-2">{children}</div>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/institucional/instituicoes-financeiras/page.tsx
@@ -0,0 +1,98 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Instituções Financeiras",
+  };
+}
+
+export default function Home() {
+  return (
+    <article className="w-full flex justify-center bg-white dark:bg-zinc-900 dark:text-white pb-2">
+      <section className="px-2 container">
+        <h1 className="font-bold uppercase text-lg md:text-2xl my-8 border-b-[1px] dark:border-zinc-800 dark:text-zinc-100 pb-2 ">
+          Institstituções Financeiras
+        </h1>
+        <div className="w-full container gap-5 pb-20">
+          <div className="grid md:grid-cols-4">
+            <LinkBancos
+              nome="BANCO GENAIL"
+              postadoEm=" 19 de março de 2024"
+              href="banco-genail"
+            />
+            <LinkBancos
+              nome="BANCO MASTER"
+              postadoEm="27 de fevereiro de 2024"
+              href="banco-master"
+            />
+            <LinkBancos
+              nome="BTG PACTUAL"
+              postadoEm="2 de outubro de 2023"
+              href="btg-pactual"
+            />
+            <LinkBancos
+              nome="PLANNER"
+              postadoEm="2 de outubro de 2023"
+              href="planner"
+            />
+            <LinkBancos
+              nome="XP INVESTIMENTOS"
+              postadoEm="2 de outubro de 2023"
+              href="xp-investimentos"
+            />
+            <LinkBancos
+              nome="BANCO SANTANDER"
+              postadoEm="2 de outubro de 2023"
+              href="banco-santander"
+            />
+            <LinkBancos
+              nome="BANCO BRADESCO"
+              postadoEm="2 de outubro de 2023"
+              href="banco-bradesco"
+            />
+            <LinkBancos
+              nome="BANCO SAFRA"
+              postadoEm="2 de outubro de 2023"
+              href="banco-safra"
+            />
+            <LinkBancos
+              nome="BANCO ITAÚ"
+              postadoEm="27 de setembro de 2023"
+              href="banco-itau"
+            />
+            <LinkBancos
+              nome="BANCO DO BRASIL"
+              postadoEm="17 de fevereiro de 2014"
+              href="banco-do-brasil"
+            />
+            <LinkBancos
+              nome="CAIXA ECONôMICA FEDERAL"
+              postadoEm="2 de janeiro de 2014"
+              href="caixa-economica-federal"
+            />
+          </div>
+        </div>
+      </section>
+    </article>
+  );
+}
+
+export const LinkBancos = ({ nome, postadoEm, href }: any) => {
+  return (
+    <div className="flex flex-col">
+      <p className="text-xl md:text-2xl uppercase pt-10 font-semibold">
+        {nome}
+      </p>
+      <p className="text-lg md:text-base font-extralight">
+        Postado em: {postadoEm}
+      </p>
+      <Link
+        href={`/institucional/instituicoes-financeiras/${href}`}
+        className=" bg-zinc-700 w-3/4 uppercase text-sm text-center text-white font-bold py-2 px-4 rounded mt-5"
+      >
+        Leia mais
+      </Link>
+    </div>
+  );
+};
