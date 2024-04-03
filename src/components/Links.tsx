6a6bac4370363b09d5ba56c7e129ed733069e480
João Victor <jvbatistagomes11@gmail.com>
2024-04-03 14:05:52 -0300

    feat: adicionado target aos links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 interface props {
   href: string;
   text: string;
+  target?: string;
 }
 
-export const LinksPage = ({ href, text }: props) => {
+export const LinksPage = ({ href, text, target }: props) => {
   return (
     <Link
+      target={target}
       href={href}
       className="flex items-center justify-center text-center shadow-md p-5 bg-zinc-100 dark:bg-zinc-800 hover:dark:bg-zinc-700 group hover:bg-zinc-200 hover:scale-105 transition-transform ease-in-out duration-300"
     >
