7676772074950678435df28aa76273e93c2832d5
João Victor <jvbatistagomes11@gmail.com>
2024-04-04 14:37:14 -0300

    style: adicionado classname no componente LinkAzul

--- src/app/institucional/components/links.tsx
+++ src/app/institucional/components/links.tsx
@@ -2,14 +2,15 @@ import Link from "next/link";
 interface props {
   href: string;
   text: string;
+  className?: string;
 }
 
-export default function LinkAzul({ href, text }: props) {
+export default function LinkAzul({ href, text, className }: props) {
   return (
     <Link
       target="_blank"
       href={href}
-      className="text-blue-500 hover:text-blue-700 hover:underline"
+      className={`text-blue-500 hover:text-blue-700 hover:underline ${className}`}
     >
       {text}
     </Link>
