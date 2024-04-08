269b4d341ae580748d86bbcc86a265a45d502e6c
João Victor <jvbatistagomes11@gmail.com>
2024-04-08 14:19:23 -0300

    chore: adicioando suporte a imagens com url "//wp-content/uploads/*/*/*.jpg"

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/wp-content/uploads/*/*/*", // Aceitará todas as imagens PNG com o caminho fornecido
       },
+      {
+        protocol: "https",
+        hostname: "aparecidaprev.go.gov.br",
+        port: "",
+        pathname: "//wp-content/uploads/*/*/*.jpg", // Aceitará todas as imagens JPG com o caminho fornecido
+      },
     ],
   },
 };
