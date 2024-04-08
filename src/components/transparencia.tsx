ffe4d2c9ebbbe2ae20c3a7ff9a858bd597c8e992
João Victor <jvbatistagomes11@gmail.com>
2024-04-08 14:18:56 -0300

    feat: adicionado links na parte transparencia

--- src/components/transparencia.tsx
+++ src/components/transparencia.tsx
@@ -7,16 +7,16 @@ export const metadata: Metadata = {
 };
 export default function Transparencia() {
   return (
-    <article className="w-full flex justify-center bg-white dark:bg-zinc-900 dark:text-zinc-200">
+    <article className="w-full flex justify-center bg-white dark:bg-zinc-900 dark:text-zinc-200 pb-20">
       <section className="px-2 container">
         <h1 className="font-bold uppercase text-lg md:text-2xl my-8">
           Transparencia
         </h1>
         <div className="grid grid-cols-2 md:grid-cols-4">
           <LinksTransparencia
-            href="/"
+            href="http://previdenciadeaparecidadegoiania.sigepnet.com.br/transparencia/folha.php"
             text="Folha de pagamento"
-            description="lorem "
+            description="Aqui você consulta e indentificar os servidores públicos dos nosso Município e suas remunerações."
           >
             <Image
               src="/mais.svg"
@@ -27,9 +27,9 @@ export default function Transparencia() {
             />
           </LinksTransparencia>
           <LinksTransparencia
-            href="/"
+            href="http://gestaopublica.previdencia.aparecida.bsit-br.com.br/portal/employee-transparency.jsf?e="
             text="Consultar Matrícula"
-            description="lorem "
+            description="Consulte matrículas através do CPF."
           >
             <Image
               src="/mais.svg"
@@ -39,16 +39,11 @@ export default function Transparencia() {
               className="w-3/4 sm:w-2/4"
             />
           </LinksTransparencia>
-          <LinksTransparencia href="/" text="Contracheque" description="lorem ">
-            <Image
-              src="/mais.svg"
-              alt="mais"
-              width={150}
-              height={150}
-              className="w-3/4 sm:w-2/4"
-            />
-          </LinksTransparencia>
-          <LinksTransparencia href="/" text="Cédula C " description="lorem ">
+          <LinksTransparencia
+            href="http://gestaopublica.previdencia.aparecida.bsit-br.com.br/portal/employee-login.jsf?e="
+            text="Contracheque"
+            description="Veja os contracheques online."
+          >
             <Image
               src="/mais.svg"
               alt="mais"
@@ -58,9 +53,22 @@ export default function Transparencia() {
             />
           </LinksTransparencia>
           <LinksTransparencia
-            href="/"
+            href="http://gestaopublica.previdencia.aparecida.bsit-br.com.br/portal/employee-login.jsf?e="
+            text="Cédula C "
+            description="Veja os comprovantes de rendimentos."
+          >
+            <Image
+              src="/mais.svg"
+              alt="mais"
+              width={150}
+              height={150}
+              className="w-3/4 sm:w-2/4"
+            />
+          </LinksTransparencia>
+          <LinksTransparencia
+            href="http://gestaopublica.previdencia.aparecida.bsit-br.com.br/portal/employee-register.jsf?e="
             text="Primeiro Acesso"
-            description="lorem "
+            description="Cadastre-se para o primeiro acesso ao portal"
           >
             <Image
               src="/mais.svg"
@@ -70,16 +78,11 @@ export default function Transparencia() {
               className="w-3/4 sm:w-2/4"
             />
           </LinksTransparencia>
-          <LinksTransparencia href="/" text="Agendamentos" description="lorem ">
-            <Image
-              src="/mais.svg"
-              alt="mais"
-              width={150}
-              height={150}
-              className="w-3/4 sm:w-2/4"
-            />
-          </LinksTransparencia>
-          <LinksTransparencia href="/" text="Webmail" description="lorem ">
+          <LinksTransparencia
+            href="https://www.aparecidaprev.com.br/portal/agendamento/admin/index.php"
+            text="Agendamentos"
+            description="Agendamento de péricia por secretaria"
+          >
             <Image
               src="/mais.svg"
               alt="mais"
@@ -89,9 +92,22 @@ export default function Transparencia() {
             />
           </LinksTransparencia>
           <LinksTransparencia
-            href="/"
+            href="https://mail.aparecida.go.gov.br"
+            text="Webmail"
+            description="Acesse o webmail de AparecidaPrev"
+          >
+            <Image
+              src="/mais.svg"
+              alt="mais"
+              width={150}
+              height={150}
+              className="w-3/4 sm:w-2/4"
+            />
+          </LinksTransparencia>
+          <LinksTransparencia
+            href="https://sigp.aparecida.go.gov.br/sig/"
             text="Sistema de Gestão Prodata"
-            description="lorem "
+            description="PRO DATA"
           >
             <Image
               src="/mais.svg"
@@ -122,8 +138,9 @@ export const LinksTransparencia = ({
 }: props) => {
   return (
     <Link
+      target="_blank"
       href={href}
-      className="flex flex-col text-center justify-center items-center transition ease-in-out duration-300 hover:text-zinc-900 hover:bg-zinc-100/95 dark:hover:bg-zinc-800 m-5 p-5 group hover:scale-110"
+      className="flex flex-col text-center items-center transition ease-in-out duration-300 hover:text-zinc-900 hover:bg-zinc-100/95 dark:hover:bg-zinc-800 m-5 p-5 group hover:scale-110"
     >
       {children}
       <p className="font-semibold text-sm uppercase sm:text-lg text-zinc-500 dark:text-zinc-100 group-hover:text-zinc-900 group-hover:dark:text-zinc-300">
