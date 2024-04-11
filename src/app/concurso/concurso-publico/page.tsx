e9a4e720617042a295dd1642ba4801ec8ac29c2b
João Victor <jvbatistagomes11@gmail.com>
2024-04-11 13:40:22 -0300

    feat: paginas concursos

---
+++ src/app/concurso/concurso-publico/page.tsx
@@ -0,0 +1,58 @@
+import LinkAzul from "@/app/institucional/components/links";
+import Container from "@/components/container";
+import { Metadata } from "next";
+import { LinkConcurso } from "../page";
+
+export const metadata: Metadata = {
+  title: "CONCURSO PÚBLICO APARECIDAPREV N°001/2017",
+};
+
+export default function Home() {
+  return (
+    <Container
+      title="CONCURSO PÚBLICO APARECIDAPREV N°001/2017"
+      className="font-light"
+    >
+      <p>
+        O Presidente do Fundo de Previdência dos Servidores do Município de
+        Aparecida de Goiânia – APARECIDAPREV –, no uso das atribuições que lhe
+        são conferidas pela legislação vigente e, em especial, o disposto da Lei
+        Orgânica do Município de Aparecida de Goiânia de 2008 e alterações
+        posteriores, na Lei Complementar Municipal N°127 de 24 de fevereiro de
+        2017 e alterações posteriores, e demais dispositivos legais aplicáveis à
+        espécie, torna pública a realização de Concurso Público para o
+        provimento de vagas do quadro permanente do Instituto de Previdência de
+        Aparecida de Goiânia, para o preenchimento de vagas, distribuídas
+        conforme consta do Anexo II deste Edital e a formação de Cadastro de
+        Reserva, para aproveitamento à medida que forem surgindo novas vagas, no
+        limite do prazo de validade deste certame, de acordo com a legislação
+        vigente e em conformidade com as normas estabelecidas no Edital
+        N°001/2017.
+      </p>
+      <br />
+      <section className="flex flex-col font-semibold pl-5">
+        <LinkConcurso text="Anexos" href="/concurso/anexos" />
+        <LinkConcurso text="Edital" href="/concurso/edital" />
+        <LinkConcurso
+          text="Editais de Convocação"
+          href="/concurso/editais-de-convocacao"
+        />
+        <LinkConcurso text="Gabaritos" href="/concurso/gabaritos" />
+        <LinkConcurso text="Resultado Final" href="/concurso/resultado-final" />
+      </section>
+      <br />
+      <p>Documentos de acesso rápido:</p>
+      <br />
+      <section className="flex flex-col font-semibold pl-5">
+        <LinkAzul
+          text="059 – 2021 – PRORROGAÇÃO DO CONCURSO"
+          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2021/03/059-2021-PRORROGACAO-DO-CONCURSO.pdf"
+        />
+        <LinkAzul
+          text="057-2020-PORTARIA-SUSPENSAO-CONCURSO"
+          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2021/03/057-2020-PORTARIA-SUSPENSAO-CONCURSO.pdf"
+        />
+      </section>
+    </Container>
+  );
+}
--- src/app/concurso/page.tsx
+++ src/app/concurso/page.tsx
@@ -1,20 +1,41 @@
 import Container from "@/components/container";
 import { Metadata } from "next";
+import LinkAzul from "../institucional/components/links";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Concursos",
 };
 
-export default function Concursos() {
+export default function Home() {
   return (
     <Container title="Concurso público" className="font-light">
       <p>
         Nesta página é possível acompanhar os assuntos em relação à realização
         de concursos públicos promovidos pelo Instituto de Previdência de
         Aparecida de Goiânia.
-      </p>{" "}
+      </p>
       <br />
-      <li>CONCURSO APARECIDAPREV N° 001/2017</li>
+      <LinkConcurso
+        text="CONCURSO APARECIDAPREV N°001/2017"
+        href="/concurso/concurso-publico"
+      />
     </Container>
+  );
+}
+
+interface props {
+  href: string;
+  text: string;
+  className?: string;
+}
+export function LinkConcurso({ href, text, className }: props) {
+  return (
+    <Link
+      href={href}
+      className={`text-blue-500 hover:text-blue-700 hover:underline ${className}`}
+    >
+      {text}
+    </Link>
   );
 }
