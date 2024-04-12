d4e7561371e88b134856023af858f151a34200b2
João Victor <jvbatistagomes11@gmail.com>
2024-04-12 13:44:47 -0300

    style: alterado imagem de fundo da home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "image-pref": "url('/bg-1.jpg')",
+        "image-pref": "url('/aparecidaprev.jpeg')",
       },
       keyframes: {},
       animation: {
