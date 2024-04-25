7ba92c9141532f906a95295499e36474e9eb48bd
João Victor <jvbatistagomes11@gmail.com>
2024-04-25 15:06:41 -0300

    feat: trocado o baseURL das apis

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-export const AxiosInstance =
-  axios.create({
-    baseURL: `https://jsonplaceholder.typicode.com/`,
-  })
+export const AxiosInstance = axios.create({
+  baseURL: `https://aparecidaprev.go.gov.br/wp-json/wp/v2`,
+});
