0b3d5d2b7edf4c522a3891fd65dfbf16a4a9ac3c
João Victor <jvbatistagomes11@gmail.com>
2024-05-02 14:40:24 -0300

    feat: intehração a api + refatoração

--- src/app/institucional/instituicoes-financeiras/[slug]/page.tsx
+++ src/app/institucional/instituicoes-financeiras/[slug]/page.tsx
@@ -1,176 +1,54 @@
-import { Metadata } from "next";
-import Image from "next/image";
+"use client";
+import {
+  extractTextFromHtml,
+  extrairLinksDoHtml,
+  formatarData,
+} from "@/utils/functions";
+import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { PostsProps } from "@/interfaces/interfaces";
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import Container from "@/components/container";
+import LinkAzul from "@/app/institucional/components/links";
+import Loading from "@/app/loading";
 
-type Props = {
+interface Props {
   params: {
     slug: string;
-  };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const title = params.slug;
-  return {
-    title: title,
   };
 }
 
 export default function Page({ params }: Props) {
-  return <GeneratePage params={params} />;
-}
+  const [posts, setPosts] = useState<PostsProps[]>();
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await AxiosInstance.get(`/posts?slug=${params.slug}`);
+        setPosts(response.data);
+      };
+      fetchData();
+    } catch (error) {
+      console.log("Erro ao buscar dados da página:", error);
+    }
+  }, [params.slug]);
 
-export const GeneratePage = ({ params }: Props) => {
-  switch (params.slug) {
-    case "banco-genail":
-      return (
-        <PaginaSlug nome="banco genail" postadoEm="19 de março de 2024">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2024/03/download-300x147.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "banco-master":
-      return (
-        <PaginaSlug nome="banco master" postadoEm="27 de fevereiro de 2024">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2024/02/Banco-Master-300x76.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "btg-pactual":
-      return (
-        <PaginaSlug nome="banco pactual" postadoEm="2 de outubro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-BTG.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "planner":
-      return (
-        <PaginaSlug nome="planner" postadoEm="2 de outubro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-planner.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "banco-santander":
-      return (
-        <PaginaSlug nome="banco santander" postadoEm="2 de outubro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-sANTANDER.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "xp-investimentos":
-      return (
-        <PaginaSlug nome="xp investimentos" postadoEm="2 de outubro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/INVESTIMENTOS.jpg"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "banco-bradesco":
-      return (
-        <PaginaSlug nome="banco bradesco" postadoEm="2 de outubro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-BRADESCO.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "banco-safra":
-      return (
-        <PaginaSlug nome="banco safra" postadoEm="2 de outubro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/10/banco-safra.png"
-            alt=""
-            width={300}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "banco-itau":
-      return (
-        <PaginaSlug nome="banco itaú" postadoEm="27 de setembro de 2023">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/09/Banco-Itau.png"
-            alt=""
-            width={200}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "banco-do-brasil":
-      return (
-        <PaginaSlug nome="banco do brasil" postadoEm="17 de fevereiro de 2014">
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2014/02/Banco-Brasil.png"
-            alt=""
-            width={200}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    case "caixa-economica-federal":
-      return (
-        <PaginaSlug
-          nome="caixa economica federal"
-          postadoEm="2 de janeiro de 2014"
-        >
-          <Image
-            src="https://aparecidaprev.go.gov.br/wp-content/uploads/2014/01/Caixa.png"
-            alt=""
-            width={200}
-            height={147}
-          />
-        </PaginaSlug>
-      );
-    default:
-      return (
-        <PaginaSlug nome="Página não encontrada" postadoEm="N/A">
-          <p>Desculpe, a página que você está procurando não foi encontrada.</p>
-        </PaginaSlug>
-      );
-  }
-};
+  console.log(posts);
 
-interface props {
-  nome: string;
-  postadoEm: string;
-  children?: React.ReactNode;
-}
+  if (posts?.length === 0) return <PaginaNaoEncontrada />;
+  if (!posts) return <Loading />;
 
-export const PaginaSlug = ({ nome, postadoEm, children }: props) => {
   return (
-    <div className="flex w-full justify-center gap-5 bg-white pb-20 dark:bg-zinc-800 dark:text-zinc-100">
-      <div className="container flex-col">
-        <p className="pt-10 text-xl font-semibold uppercase md:text-2xl">
-          {nome}
-        </p>
-        <p className="text-lg font-extralight md:text-base">
-          Postado em: {postadoEm}
-        </p>
-        <div className="p-2 pt-4">{children}</div>
-      </div>
+    <div>
+      {posts?.map((post) => (
+        <Container
+          title={extractTextFromHtml(post.title.rendered)}
+          key={post.id}
+          className="grid max-w-xl"
+          PostadoEm={formatarData(post.date)}
+        >
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </Container>
+      ))}
     </div>
   );
-};
+}
--- src/app/institucional/instituicoes-financeiras/page.tsx
+++ src/app/institucional/instituicoes-financeiras/page.tsx
@@ -1,92 +1,48 @@
+"use client";
+import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { PostsProps } from "@/interfaces/interfaces";
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
 import Container from "@/components/container";
-import { Metadata } from "next";
-import Link from "next/link";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Instituções Financeiras",
-  };
-}
+import Loading from "@/app/loading";
+import Cards from "@/components/card";
 
 export default function Home() {
+  const [postsDpir, setPostsDpir] = useState<PostsProps[]>([]);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await AxiosInstance.get("/posts?categories=15");
+        setPostsDpir(response.data);
+        const responsePage2 = await AxiosInstance.get(
+          "/posts?categories=15&page=2",
+        );
+        setPostsDpir((prevPosts) => [...prevPosts, ...responsePage2.data]);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  if (!postsDpir) return <Loading />;
+  if (postsDpir.length === 0) return <PaginaNaoEncontrada />;
+
   return (
-    <Container title="Instituções Financeiras">
-      <div className="grid md:grid-cols-4">
-        <LinkBancos
-          nome="BANCO GENAIL"
-          postadoEm=" 19 de março de 2024"
-          href="banco-genail"
+    <Container
+      title="DIPR – Demostrativo de Informações Prev. e Repasse"
+      className="grid md:grid-cols-2"
+    >
+      {postsDpir?.map((post, i) => (
+        <Cards
+          key={i}
+          title={post.title.rendered}
+          postadoEm={post.date}
+          descrição={post.excerpt.rendered}
+          href={`/institucional/instituicoes-financeiras/${post.slug}`}
         />
-        <LinkBancos
-          nome="BANCO MASTER"
-          postadoEm="27 de fevereiro de 2024"
-          href="banco-master"
-        />
-        <LinkBancos
-          nome="BTG PACTUAL"
-          postadoEm="2 de outubro de 2023"
-          href="btg-pactual"
-        />
-        <LinkBancos
-          nome="PLANNER"
-          postadoEm="2 de outubro de 2023"
-          href="planner"
-        />
-        <LinkBancos
-          nome="XP INVESTIMENTOS"
-          postadoEm="2 de outubro de 2023"
-          href="xp-investimentos"
-        />
-        <LinkBancos
-          nome="BANCO SANTANDER"
-          postadoEm="2 de outubro de 2023"
-          href="banco-santander"
-        />
-        <LinkBancos
-          nome="BANCO BRADESCO"
-          postadoEm="2 de outubro de 2023"
-          href="banco-bradesco"
-        />
-        <LinkBancos
-          nome="BANCO SAFRA"
-          postadoEm="2 de outubro de 2023"
-          href="banco-safra"
-        />
-        <LinkBancos
-          nome="BANCO ITAÚ"
-          postadoEm="27 de setembro de 2023"
-          href="banco-itau"
-        />
-        <LinkBancos
-          nome="BANCO DO BRASIL"
-          postadoEm="17 de fevereiro de 2014"
-          href="banco-do-brasil"
-        />
-        <LinkBancos
-          nome="CAIXA ECONôMICA FEDERAL"
-          postadoEm="2 de janeiro de 2014"
-          href="caixa-economica-federal"
-        />
-      </div>
+      ))}
     </Container>
   );
 }
-
-export const LinkBancos = ({ nome, postadoEm, href }: any) => {
-  return (
-    <div className="flex flex-col">
-      <p className="pt-10 text-xl font-semibold uppercase md:text-2xl">
-        {nome}
-      </p>
-      <p className="text-lg font-extralight md:text-base">
-        Postado em: {postadoEm}
-      </p>
-      <Link
-        href={`/institucional/instituicoes-financeiras/${href}`}
-        className=" hover mt-5 w-3/4 rounded bg-zinc-700 px-4 py-2 text-center text-sm font-bold uppercase text-white transition-all duration-300 ease-out hover:bg-zinc-800/50"
-      >
-        Leia mais
-      </Link>
-    </div>
-  );
-};
