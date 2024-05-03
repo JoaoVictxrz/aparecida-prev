0ed82315b1a4983c83839d8e65166e58f2090a1d
João Victor <jvbatistagomes11@gmail.com>
2024-05-03 14:26:53 -0300

    feat: layout para alterar metadata

---
+++ src/app/financeiro/apr-autorizacao-de-aplicacao-e-resgate/layout.tsx
@@ -0,0 +1,6 @@
+export const metadata = {
+  title: "APR – Autorização de Aplicação e Resgate",
+};
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
