e00bdac61447fbb14bac0aecb95bb315051f1672
João Victor <jvbatistagomes11@gmail.com>
2024-05-06 13:30:14 -0300

    refactor: integração com api, refatorado e layout para metadata

--- src/app/financeiro/demonstrativo-variacao-patrimonial/page.tsx
+++ src/app/financeiro/demonstrativo-variacao-patrimonial/page.tsx
@@ -1,104 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { PostsProps } from "@/interfaces/interfaces";
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
 import Container from "@/components/container";
-import CardsFinanceiro from "../components/cards";
-import { Metadata } from "next";
-import { getQueryParams } from "../components/getQueryParams";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Demonstrativo Variação Patrimonial ",
-  };
-}
-
-const links = [
-  {
-    title: "Demonstrativo da Variação Patrimonial 2024",
-    postadoEm: "15 de fevereiro de 2024",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2024",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2023",
-    postadoEm: "5 de junho de 2023",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2023",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2022",
-    postadoEm: "19 de abril de 2022",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2022",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2021",
-    postadoEm: "24 de setembro de 2021",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2021",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2020",
-    postadoEm: "3 de julho de 2020",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2020",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2019",
-    postadoEm: "16 de agosto de 2019",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2019",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2018",
-    postadoEm: "12 de junho de 2018",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2018",
-  },
-  {
-    title: "Demonstrativo da Variação Patrimonial 2017",
-    postadoEm: "13 de junho de 2017",
-    descrição: "",
-    href: "demonstrativo-da-variacao-patrimonial-2017",
-  },
-  {
-    title: "Demonstrativo Variação Patrimonial 2016",
-    postadoEm: "17 de outubro de 2016",
-    descrição: "",
-    href: "demonstrativo-variacao-patrimonial-2016",
-  },
-  {
-    title: "Variação Patrimonial 2015",
-    postadoEm: "28 de agosto de 2015",
-    descrição: "",
-    href: "variacao-patrimonial-2015",
-  },
-  {
-    title: "Variação Patrimonial 2014",
-    postadoEm: "6 de maio de 2014",
-    descrição: "",
-    href: "variacao-patrimonial-2014",
-  },
-  {
-    title: "Demonstrativo das Variações Patrimoniais 2013",
-    postadoEm: "4 de fevereiro de 2014",
-    descrição: "",
-    href: "demonstrativo-das-variacoes-patrimoniais-2013",
-  },
-];
+import Loading from "@/app/loading";
+import Cards from "@/components/card";
 
 export default function Home() {
+  const [posts, setPosts] = useState<PostsProps[]>([]);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await AxiosInstance.get("/posts?categories=21");
+        setPosts(response.data);
+        const responsePage2 = await AxiosInstance.get(
+          "/posts?categories=21&page=2",
+        );
+        setPosts((prevPosts) => [...prevPosts, ...responsePage2.data]);
+      };
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  if (posts.length === 0) return <Loading />;
+
   return (
     <Container
-      title="Demonstrativo Variação Patrimonial "
+      title="Demonstrativo de Variação Patrimonial"
       className="grid md:grid-cols-2"
     >
-      {links.map((link, i) => (
-        <CardsFinanceiro
+      {posts?.map((post, i) => (
+        <Cards
           key={i}
-          title={link.title}
-          postadoEm={link.postadoEm}
-          descrição={link.descrição}
-          href={link.href}
-          path="demonstrativo-variacao-patrimonial"
-          query={getQueryParams(link)}
+          title={post.title.rendered}
+          postadoEm={post.date}
+          descrição={post.excerpt.rendered}
+          href={`/financeiro/demonstrativo-variacao-patrimonial/${post.slug}`}
         />
       ))}
     </Container>
