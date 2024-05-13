07997e1a2b78ee3db42bcedf3d21aed113830f22
João Victor <jvbatistagomes11@gmail.com>
2024-05-13 15:43:14 -0300

    refactor: integração a api + refatoração

---
+++ src/app/institucional/prova-anual-de-vida/layout.tsx
@@ -0,0 +1,7 @@
+export const metada = {
+  title: "Prova Anual de Vida | Aparecida Prev",
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
--- src/app/institucional/prova-anual-de-vida/page.tsx
+++ src/app/institucional/prova-anual-de-vida/page.tsx
@@ -1,26 +1,35 @@
-import { Metadata } from "next";
+"use client";
+import { useEffect, useState } from "react";
 import LinkAzul from "../components/links";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Prova anual de vida",
-  };
-}
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
+import { extrairLinksDoHtml } from "@/utils/functions";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+
+  const fetchData = async () => {
+    const response = await AxiosInstance.get("/pages/6714");
+    setData(response.data);
+  };
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log("Erro ao buscar dados: " + error);
+    }
+  }, []);
+
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
   return (
-    <Container title="Prova anual de vida">
-      <div className="flex flex-col pl-5">
-        <LinkAzul
-          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/07/DECRETO-N-620-DE-22-DE-AGOSTO-2022.pdf"
-          text="Decreto “N” N° 620 de 22 de agosto de 2022"
-        />
-        <LinkAzul
-          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Portaria-N%C2%B0-275-de-29-de-julho-de-2022.pdf"
-          text="Portaria N° 275, de 29 de julho de 2022"
-        />
-      </div>
+    <Container title={data?.title.rendered!} className="space-y-2">
+      {links.map((link, i) => (
+        <div key={i} className="line-clamp-1 md:pl-5">
+          <LinkAzul href={link.url} text={link.text} className="w-3/4" />
+        </div>
+      ))}
     </Container>
   );
 }
