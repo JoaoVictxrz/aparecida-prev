507f4792b5a31c151d0ec8a3f4e110e3844226b3
João Victor <jvbatistagomes11@gmail.com>
2024-05-13 15:50:36 -0300

    refactor: integração a api + refetaroção

--- src/app/sobre/organograma/page.tsx
+++ src/app/sobre/organograma/page.tsx
@@ -1,22 +1,28 @@
+"use client";
 import Container from "@/components/container";
-import { Metadata } from "next";
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
 import Image from "next/image";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Organograma",
-  };
-}
+import { useEffect, useState } from "react";
 
 export default function Organograma() {
+  const [data, setData] = useState<PostsProps>();
+
+  const fetchData = async () => {
+    const response = await AxiosInstance.get("/pages/6609");
+    setData(response.data);
+  };
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log("Erro ao buscar dados: " + error);
+    }
+  }, []);
+
   return (
-    <Container title="Organograma" className="flex justify-center">
-      <Image
-        src="/sobre/Organograma.jpg"
-        alt="Organograma"
-        width={1000}
-        height={1000}
-      />
+    <Container title={data?.title.rendered!} className="flex justify-center">
+      <div dangerouslySetInnerHTML={{ __html: data?.content.rendered! }}></div>
     </Container>
   );
 }
