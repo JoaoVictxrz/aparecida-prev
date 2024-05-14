d8bacd3dd7fd1552799e84474c025cf392d382e1
João Victor <jvbatistagomes11@gmail.com>
2024-05-14 15:16:24 -0300

    refactor: integração com a api + refatoração

--- src/app/institucional/audiencia-publica/page.tsx
+++ src/app/institucional/audiencia-publica/page.tsx
@@ -1,44 +1,47 @@
-import { Metadata } from "next";
+"use client";
+import { useEffect, useState } from "react";
 import LinkAzul from "../components/links";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Audiência publica",
-  };
-}
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
+import { extrairLinksDoHtml } from "@/utils/functions";
+import { text } from "stream/consumers";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const fetchData = async () => {
+    const response = await AxiosInstance.get("/pages/6515");
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log("Erro ao buscar dados da página:", error);
+    }
+  }, []);
+
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
   return (
     <Container title="Audiência publica">
-      <div className="flex flex-col items-start text-center gap-2 pb-5 border-b-[1px] dark:border-zinc-800">
+      <div className="flex flex-col items-start gap-2 border-b-[1px] pb-5 dark:border-zinc-800">
         <p className="font-semibold uppercase">Audiencia pública n°002/2023</p>
-        <div className="flex flex-col items-start text-center pl-5 gap-1">
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/07/Edital-de-Convocacao-de-Audiencia-Publica.pdf"
-            text="Edital de Convocação AudiÊncia Pública N°002/2023"
-          />
-          <LinkAzul
-            href="/institucional/audiencia-publica/audiencia-publica-2023"
-            text="Divulgação da Audiência"
-          />
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/07/ATA-E-LISTA-DE-PRESENCA.pdf"
-            text="Ata e Lista de Presença"
-          />
-          <LinkAzul
-            href="/institucional/audiencia-publica/audiencia-publica-de-prestacao-de-contas-2022-e-realizada"
-            text="Audiência Pública de Prestação de Contas 2022 é realizada"
-          />
+        <div className="flex flex-col items-start gap-1 pl-5">
+          {links.map(
+            (link, i) =>
+              i < 4 && <LinkAzul key={i} href={link.url} text={link.text} />,
+          )}
         </div>
       </div>
-      <div className="flex flex-col items-start text-center gap-2 py-5 border-b-[1px] dark:border-zinc-800">
+      <div className="flex flex-col items-start gap-2 border-b-[1px] py-5 dark:border-zinc-800">
         <p className="font-semibold uppercase">Audiencia pública n°001/2023</p>
-        <div className="flex flex-col items-start text-center pl-5 gap-1">
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/04/Edital-de-Convocacao-e-Publicacao.pdf"
-            text="Edital de Convocação AudiÊncia Pública N°001/2023 Eleição 2023"
-          />
+        <div className="flex flex-col items-start gap-1 pl-5">
+          {links.map(
+            (link, i) =>
+              i >= 4 && <LinkAzul key={i} href={link.url} text={link.text} />,
+          )}
         </div>
       </div>
     </Container>
