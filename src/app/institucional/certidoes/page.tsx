e0d02810fd419924fa4fb15e5c4a8e9aa002332a
João Victor <jvbatistagomes11@gmail.com>
2024-05-15 15:46:47 -0300

    refactor: integração com api + refatoração

--- src/app/institucional/certidoes/page.tsx
+++ src/app/institucional/certidoes/page.tsx
@@ -1,36 +1,32 @@
-import { Metadata } from "next";
+"use client";
+import { PostsProps } from "@/interfaces/interfaces";
 import LinkAzul from "../components/links";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Certidões",
-  };
-}
+import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { extrairLinksDoHtml } from "@/utils/functions";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get<PostsProps>("/pages/5923");
+      setData(response.data);
+    } catch (error) {
+      console.log("erro ao buscar dados: ", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
   return (
-    <Container title="Certidões" className="flex flex-col">
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/12/Certidao-Negativa-de-Debitos_APARECIDAPREV-SEFAZ.pdf"
-        text="CERTIDÃO NEGATIVA DE DÉBITOS – ESTADO"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/12/Consulta-Regularidade-do-Empregador_ATUALIZADA.pdf"
-        text="CERTIDÃO NEGATIVA DE DÉBITOS – FGTS"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/12/certidao-receita-federal-1.pdf"
-        text="CERTIDÃO NEGATIVA DE DÉBITOS – UNIÃO"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/07/Trabalhista.pdf"
-        text="CERTIDÃO NEGATIVA DE DÉBITOS – TRABALHISTA"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/12/Certidao-Negativa-Municipal_ATUALIZADA.pdf"
-        text="CERTIDÃO NEGATIVA DE DÉBITOS – MUNICIPIO"
-      />
+    <Container title={data?.title.rendered!} className="flex flex-col">
+      {links.map((link, i) => (
+        <LinkAzul href={link.url} text={link.text} key={i} className="pl-5" />
+      ))}
     </Container>
   );
 }
