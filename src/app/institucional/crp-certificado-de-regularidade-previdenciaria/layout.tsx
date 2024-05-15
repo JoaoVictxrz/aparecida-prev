19b40dd417d6b1d59ab9f5fd6a8d7626893efdc4
João Victor <jvbatistagomes11@gmail.com>
2024-05-15 16:36:28 -0300

    refactor: adicionado metadata no layout de crp - certficado de regularidade

--- src/app/institucional/crp-certificado-de-regularidade-previdenciaria/layout.tsx
+++ src/app/institucional/crp-certificado-de-regularidade-previdenciaria/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "CRP - Certificado de Regularidade Previdênciária",
+  };
+}
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
