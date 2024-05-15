8ed4d62f89bfaf79a24c83cfcbc77ec6f6172248
João Victor <jvbatistagomes11@gmail.com>
2024-05-15 14:23:57 -0300

    feat: integração com api + refatoração

--- src/app/institucional/governanca-corporativa/page.tsx
+++ src/app/institucional/governanca-corporativa/page.tsx
@@ -1,60 +1,38 @@
-import { Metadata } from "next";
-import LinkAzul from "../components/links";
+"use client";
+import { useEffect, useState } from "react";
+import { extrairLinksDoHtml } from "@/utils/functions";
+import { AxiosInstance } from "@/services/axios";
+import { PostsProps } from "@/interfaces/interfaces";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Governança corporativa",
-  };
-}
+import LinkAzul from "../components/links";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/6016");
+      setData(response.data);
+    } catch (error) {
+      console.log("erro ao buscar dados: " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
   return (
-    <Container title="Governânça corporativa" className="flex flex-col">
-      <span className="font-bold uppercase">
+    <Container title={data?.title.rendered!} className="flex flex-col pl-5">
+      {links.map((link, i) => (
         <LinkAzul
-          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/12/Relatorio-de-Governanca-Corporativa-01-2023-Assinado-1.pdf"
-          text="RELATÓRIO DE GOVERNANÇA CORPORATIVA 1º SEMESTRE DE 2023"
+          key={i}
+          href={link.url}
+          text={link.text}
+          className={`${link.text.includes("RELATÓRIO") ? "font-semibold" : ""}`}
         />
-      </span>
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/12/Ata-de-novembro-APROVACAO-DO-RELATORIO-GOVERNANCA.pdf"
-        text="Ata Novembro 2023 – Conselho Previdência"
-      />
-      <span className="font-bold uppercase">
-        <LinkAzul
-          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/2.1.3-RELATORIO-DE-GOVERNANCA-CORPORATIVA-2o-SEMESTRE-DE-2022.pdf"
-          text="RELATÓRIO DE GOVERNANÇA CORPORATIVA 2º SEMESTRE DE 2022"
-        />
-      </span>
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Resolucao-n%C2%B0-001-2023-Conselho-Fiscal.pdf"
-        text="Resolução n° 001-2023 – Conselho Fiscal"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Resolucao-n%C2%B0-001-2023-Conselho-Previdencia.pdf"
-        text="Resolução n° 001-2023 Conselho Previdência"
-      />
-      <span className="font-bold uppercase">
-        <LinkAzul
-          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/2.1.2-RELATORIO-DE-GOVERNANCA-CORPORATIVA-1o-SEMESTRE-DE-2022.pdf"
-          text="RELATÓRIO DE GOVERNANÇA CORPORATIVA 1º SEMESTRE DE 2022"
-        />
-      </span>
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Resolucao-n%C2%B0-001-2022-Conselho-Fiscal.pdf"
-        text="Resolução n° 001-2022 – Conselho Fiscal"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Resolucao-n%C2%B0-004-2022-Conselho-de-Previdencia.pdf"
-        text="Resolução n° 004-2022 – Conselho de Previdência"
-      />
-      <span className="font-bold uppercase">
-        <LinkAzul
-          href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/2.1.1-RELATORIO-DE-GOVERNANCA-CORPORATIVA-2017-A-2021.pdf"
-          text="RELATÓRIO DE GOVERNANÇA CORPORATIVA – 2017 A 2021"
-        />
-      </span>
+      ))}
     </Container>
   );
 }
