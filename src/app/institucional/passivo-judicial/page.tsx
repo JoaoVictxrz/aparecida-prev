9d547529e52cd6804132c1355a1e8f98029a2f5e
João Victor <jvbatistagomes11@gmail.com>
2024-05-15 15:40:57 -0300

    refactor: integração com api + refatoração

--- src/app/institucional/passivo-judicial/page.tsx
+++ src/app/institucional/passivo-judicial/page.tsx
@@ -1,20 +1,33 @@
-import { Metadata } from "next";
+"use client";
+import { PostsProps } from "@/interfaces/interfaces";
 import LinkAzul from "../components/links";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Passivo Judicial",
-  };
-}
+import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { extrairLinksDoHtml } from "@/utils/functions";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/5958");
+      setData(response.data);
+    } catch (error) {
+      console.log("erro ao buscar dados: " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
   return (
-    <Container title="Passivo Judicial">
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/RELATORIO-DE-PASSIVO-JUDICIAL-2022-E-2023.pdf"
-        text="Relatório Passivo Judicial – 2022 E 2023"
-      />
+    <Container title={data?.title.rendered!}>
+      {links.map((link, i) => (
+        <LinkAzul href={link.url} text={link.text} key={i} className="pl-5" />
+      ))}
     </Container>
   );
 }
