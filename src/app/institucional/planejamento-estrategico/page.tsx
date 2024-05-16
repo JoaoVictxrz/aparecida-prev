b34dfa99d80c22569e41164ae38c3ed10492a318
João Victor <jvbatistagomes11@gmail.com>
2024-05-16 16:43:08 -0300

    refactor: integração com api + refatoração

--- src/app/institucional/planejamento-estrategico/page.tsx
+++ src/app/institucional/planejamento-estrategico/page.tsx
@@ -1,44 +1,52 @@
-import { Metadata } from "next";
+"use client";
+import { PostsProps } from "@/interfaces/interfaces";
 import LinkAzul from "../components/links";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Planejamento estratégico",
-  };
-}
+import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { extrairLinksDoHtml } from "@/utils/functions";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/3283");
+      setData(response.data);
+    } catch (error) {
+      console.log("erro ao buscar dados: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
   return (
     <Container title="Planejamento estrategico">
       <div className="gap-2 border-b-[1px] dark:border-zinc-800">
         <p className="font-bold uppercase">planejamento</p>
         <div className="flex flex-col gap-1 pb-2 pl-5 pt-2">
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/PLANEJAMENTO-ESTRATEGICO-APARECIDAPREV-2019-2023.pdf"
-            text="PLANEJAMENTO ESTRATÉGICO APARECIDAPREV – 2019-2023"
-          />
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/PLANEJAMENTO-ESTRATEGICO-APARECIDAPREV-2019-2023.pdf"
-            text="PLANEJAMENTO ESTRATÉGICO APARECIDAPREV – 2018"
-          />
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2015/07/Planejamento-Estrat%C3%A9gico-do-AparecidaPrev-2015.pdf"
-            text="PLANEJAMENTO ESTRATÉGICO APARECIDAPREV – 2015"
-          />
+          {links.map((link, i) => (
+            <div key={i}>
+              {link.text.includes("PLANEJAMENTO") && (
+                <LinkAzul text={link.text} href={link.url} />
+              )}
+            </div>
+          ))}
         </div>
       </div>
       <div className="gap-2 pt-2">
         <p className="font-bold uppercase">plano de ação</p>
         <div className="flex flex-col gap-1 pl-5 pt-2">
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/07/Plano-de-Acao-Anual-2023.pdf"
-            text="PLANO DE AÇÃO ANUAL – 2023"
-          />
-          <LinkAzul
-            href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/05/PLANO-DE-ACAO-ANUAL-2022.pdf"
-            text="PLANO DE AÇÃO ANUAL – 2022"
-          />
+          {links.map((link, i) => (
+            <div key={i}>
+              {link.text.includes("PLANO") && (
+                <LinkAzul text={link.text} href={link.url} />
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </Container>
