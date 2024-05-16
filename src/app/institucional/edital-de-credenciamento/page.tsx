aa18258b7fd222093efea683ebe3368e54027456
João Victor <jvbatistagomes11@gmail.com>
2024-05-16 13:59:32 -0300

    refactor: adicionado enpoint pages da api e refatorado o codigo

--- src/app/institucional/edital-de-credenciamento/page.tsx
+++ src/app/institucional/edital-de-credenciamento/page.tsx
@@ -1,26 +1,83 @@
 "use client";
-import Container from "@/components/container";
-import { PostsProps } from "@/interfaces/interfaces";
-import { AxiosInstance } from "@/services/axios";
 import { useEffect, useState } from "react";
+import { AxiosInstance } from "@/services/axios";
+import { PostsProps } from "@/interfaces/interfaces";
+import Container from "@/components/container";
+import Loading from "@/app/loading";
+import { extrairLinksDoHtml } from "@/utils/functions";
+import LinkAzul from "../components/links";
 
 export default function Home() {
-  const [data, setData] = useState<PostsProps[]>([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await AxiosInstance.get("/posts?categories=25");
+  const [data, setData] = useState<PostsProps>();
+
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/1824");
       setData(response.data);
-    };
+    } catch (error) {
+      console.log("Erro ao buscar dados :", error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
+  const links = extrairLinksDoHtml(data?.content.rendered!);
+
+  const bancos = [
+    "BB",
+    "Caixa",
+    "Bradesco",
+    "BTG",
+    "Itaú",
+    "Safra",
+    "Santander",
+    "Planner",
+    "XP",
+  ];
+
   return (
-    <Container title="Edital de credenciamento">
-      {data.map((post, i) => (
-        <div
-          key={i}
-          dangerouslySetInnerHTML={{ __html: post.content.rendered }}
-        />
+    <Container title={data?.title.rendered!} className="space-y-2">
+      <p className="font-bold">EDITAL DE CREDENCIAMENTO 2024</p>
+      {links.map((link, i) => (
+        <div key={i} className="pl-5">
+          {link.text.includes("2024") && (
+            <LinkAzul href={link.url} text={link.text} />
+          )}
+        </div>
+      ))}
+      <p className="font-bold">EDITAL DE CREDENCIAMENTO 2023</p>
+      {links.map((link, i) => (
+        <div key={i} className="pl-5">
+          {link.text.includes("2023") && (
+            <LinkAzul href={link.url} text={link.text} />
+          )}
+        </div>
+      ))}
+      <p className="font-bold">EDITAL DE CREDENCIAMENTO 2022</p>
+      {links.map((link, i) => (
+        <div key={i} className="pl-5">
+          {link.text.includes("001-2022") && (
+            <LinkAzul href={link.url} text={link.text} />
+          )}
+        </div>
+      ))}
+      <em className="font-bold">– INSTITUIÇÕES CREDENCIADAS</em>
+      {links.map((link, i) => (
+        <div key={i} className="pl-5">
+          {bancos.some((banco) => link.text.includes(banco)) && (
+            <LinkAzul href={link.url} text={link.text} />
+          )}
+        </div>
+      ))}
+      <p className="font-bold">EDITAL DE CREDENCIAMENTO CUSTÓDIA 2022</p>
+      {links.map((link, i) => (
+        <div key={i} className="pl-5">
+          {link.text.includes("002") && (
+            <LinkAzul href={link.url} text={link.text} />
+          )}
+        </div>
       ))}
     </Container>
   );
