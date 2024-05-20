6358cdf27db1e5747a2a52689b66625c73cec10b
João Victor <jvbatistagomes11@gmail.com>
2024-05-20 13:54:14 -0300

    style: retirado height do tamnho para não pegar a tela inteira

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 "use client";
 export default function Loading() {
   return (
-    <div className="flex h-screen items-center justify-center bg-white dark:bg-zinc-900">
+    <div className="flex items-center justify-center bg-white dark:bg-zinc-900">
       <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-gray-900 dark:border-white"></div>
     </div>
   );
