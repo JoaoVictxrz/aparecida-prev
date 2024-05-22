7c4aaa74d39e58604d440d79b2aa70ce05ef0772
João Victor <jvbatistagomes11@gmail.com>
2024-05-22 16:29:52 -0300

    refactor: integração com + refatoração

--- src/app/beneficio/aposentadoria-compulsoria/page.tsx
+++ src/app/beneficio/aposentadoria-compulsoria/page.tsx
@@ -1,48 +1,37 @@
+"use client";
+import Loading from "@/app/loading";
 import Container from "@/components/container";
-import { Metadata } from "next";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Aposentadoria compulsória",
-  };
-}
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/2783");
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) return <PaginaNaoEncontrada />;
+  if (loading) return <Loading />;
+
   return (
-    <Container title="Aposentadoria Compulsoria" className="space-y-4">
-      <p>
-        <span className="font-bold">Art. 16 – </span>O Segurado Ativo será
-        automaticamente aposentado ao completar setenta anos de idade, com
-        proventos proporcionais ao tempo de contribuição calculados segundo o
-        disposto no art. 25 desta Lei.
-      </p>
-      <p>
-        <span className="font-bold">§1º – </span>A aposentadoria será declarada
-        por ato do Prefeito Municipal, com vigência a partir do dia imediato
-        àquele em que o servidor atingir a idade limite de permanência no
-        serviço.
-      </p>
-      <p>
-        <span className="font-bold">§2º – </span>A aposentadoria compulsória
-        independe de requerimento, devendo ser declarada ex-ofício pelo Prefeito
-        Municipal.
-      </p>
-      <p>
-        <span className="font-bold">§3º – </span>A contagem do tempo de
-        contribuição do servidor para cálculo dos proventos somente se dará até
-        a data em que completar 70 (setenta) anos de idade.
-      </p>
-      <p>
-        <span className="font-bold">§4º – </span>As vantagens pecuniárias
-        somente serão computadas para efeito de cálculo dos proventos se
-        adquiridas antes da data em que completar 70 (setenta) anos de idade.
-      </p>
-      <p>
-        <span className="font-bold">Art. 17 – </span>Aos servidores que, após o
-        implemento da idade limite para permanência no serviço público, tenham
-        sido mantidos em exercício de cargo de provimento efetivo, deverá ser
-        concedida a aposentadoria imediatamente.
-      </p>
+    <Container title="Aposentadoria Compulsoria">
+      <div dangerouslySetInnerHTML={{ __html: data?.content.rendered! }} />
     </Container>
   );
 }
