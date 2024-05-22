e447076c652003913d0fbdd8476b21c91b9e7f6b
João Victor <jvbatistagomes11@gmail.com>
2024-05-22 15:43:08 -0300

    refactor: integração com api + refatoração

--- src/app/conselho/membros-do-conselho/page.tsx
+++ src/app/conselho/membros-do-conselho/page.tsx
@@ -1,59 +1,36 @@
+"use client";
+import Loading from "@/app/loading";
 import Container from "@/components/container";
-import { Metadata } from "next";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Membros dos conselhos",
-  };
-}
-
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/2789");
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) return <PaginaNaoEncontrada />;
+  if (loading) return <Loading />;
+
   return (
     <Container title="Membros dos conselhos">
-      <div className="flex flex-col gap-2">
-        <strong className="font-bold uppercase underline">
-          CONSELHO MUNICIPAL DE PREVIDÊNCIA
-        </strong>
-        <h2 className="font-bold dark:text-zinc-200">TITULARES</h2>
-        <div className="flex flex-col">
-          <p className="font-light">Delson Vieira dos Santos</p>
-          <p className="font-light">Leandro Júnior Maurilio da Silva</p>
-          <p className="font-light">Einstein Almeida Ferreira Paniago</p>
-          <p className="font-light">Ereni de Araujo Almeida</p>
-          <p className="font-light">Joaci Barbosa de Almeida</p>
-          <p className="font-light">Julio Cesar Chagas Mendes</p>
-          <p className="font-light">Hellen Cassia Macedo Silva</p>
-          <p className="font-light">Maria Marta da Silva de Carvalho</p>
-          <p className="font-light">Marise Ramos de Morais</p>
-          <p className="font-light">Mauro Cesar Reges da Silva</p>
-          <p className="font-light">Renato Marcos da Silva</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-2 pt-4">
-        <strong className="font-bold uppercase underline">
-          CONSELHO FISCAL
-        </strong>
-        <h2 className="font-bold dark:text-zinc-200">TITULARES</h2>
-        <div className="flex flex-col">
-          <p className="font-light">Alzeni Cardoso Cirqueira</p>
-          <p className="font-light">Jean Jacques Martins Bastos</p>
-          <p className="font-light">Kamila Ferreira Carcute</p>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-2 pt-4">
-        <strong className="font-bold uppercase underline">
-          COMITÊ DE INVESTIMENTOS
-        </strong>
-        <div className="flex flex-col">
-          <p className="font-light">Cristiano Curado Abrantes Caetano</p>
-          <p className="font-light">Einstein Almeida Ferreira Paniago</p>
-          <p className="font-light">Julio Cesar Chagas Mendes</p>
-          <p className="font-light">Khayo Eduardo Pires de Oliveira</p>
-          <p className="font-light">Márcio Gomes Costa</p>
-        </div>
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: data?.content.rendered! }} />
     </Container>
   );
 }
