f2b93a9945b3559a2f272ac14f686841a1f79f2f
João Victor <jvbatistagomes11@gmail.com>
2024-05-22 16:10:24 -0300

    refactor: integração com api + refatoração

--- src/app/beneficio/aposentadoria-por-idade/page.tsx
+++ src/app/beneficio/aposentadoria-por-idade/page.tsx
@@ -1,39 +1,37 @@
+"use client";
+import Loading from "@/app/loading";
 import Container from "@/components/container";
-import { Metadata } from "next";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Aposentadoria por idade",
-  };
-}
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps>();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get("/pages/2784");
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) return <PaginaNaoEncontrada />;
+  if (loading) return <Loading />;
+
   return (
     <Container title="Aposentadoria por idade" className="space-y-4">
-      <p>
-        <span className="font-bold">Art. 19 – </span>O segurado fará jus à
-        aposentadoria por idade, com proventos proporcionais ao tempo de
-        contribuição, desde que preencha, cumulativamente, os seguintes
-        requisitos:
-      </p>
-      <p>
-        <span className="font-bold">I – </span>tempo mínimo de dez anos de
-        exercício no serviço público;
-      </p>
-      <p>
-        <span className="font-bold">II – </span>tempo mínimo de cinco anos de
-        efetivo exercício no cargo em que se dará a aposentadoria; e
-      </p>
-      <p>
-        <span className="font-bold">III – </span>sessenta e cinco anos de idade,
-        se homem, e sessenta anos de idade, se mulher.
-      </p>
-      <p>
-        <span className="font-bold">Parágrafo único – </span>Para o cálculo dos
-        proventos proporcionais será considerado um trinta e cinco avos por ano
-        de contribuição, se homem, e um trinta avos, se mulher e observado o que
-        dispõem o art. 25.
-      </p>
+      <div dangerouslySetInnerHTML={{ __html: data?.content.rendered! }} />
     </Container>
   );
 }
