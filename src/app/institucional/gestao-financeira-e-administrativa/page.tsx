3bb3f49c6c2a6d6a842eb5960165aca9266009d2
João Victor <jvbatistagomes11@gmail.com>
2024-05-29 15:16:55 -0300

    feat: integração com api + refatoração

--- src/app/institucional/gestao-financeira-e-administrativa/page.tsx
+++ src/app/institucional/gestao-financeira-e-administrativa/page.tsx
@@ -1,30 +1,48 @@
-import { Metadata } from "next";
-import LinkAzul from "../components/links";
+"use client";
+import Loading from "@/app/loading";
 import Container from "@/components/container";
-
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Gestão financeira e administrativa",
-  };
-}
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import { PostsProps } from "@/interfaces/interfaces";
+import { AxiosInstance } from "@/services/axios";
+import cheerio, { CheerioAPI } from "cheerio";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<PostsProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await AxiosInstance.get(
+        "/pages?slug=gestao-financeira-e-administrativa",
+      );
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.log("Erro ao buscar dados: " + error);
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) return <PaginaNaoEncontrada />;
+  if (loading) return <Loading />;
+
+  const $: CheerioAPI = cheerio.load(data[0]?.content.rendered!);
+  $("a").addClass("pl-5 text-blue-500 hover:underline hover:text-blue-700");
+  $("a").attr("target", "_blank");
+  const updatedHTML = $.html();
+
   return (
-    <Container
-      title="Gestão financeira e administrativa"
-      className="flex flex-col"
-    >
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Gestao-2017-a-2022.pdf"
-        text="Relatório de Gestão Financeira e Administrativa 2017 a 2022"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br/wp-content/uploads/2023/08/Gestao-2017-a-2021-Atualizado.pdf"
-        text="Relatório de Gestão Financeira e Administrativa 2017 a 2021"
-      />
-      <LinkAzul
-        href="https://aparecidaprev.go.gov.br//wp-content/uploads/2015/07/gestao.pdf"
-        text="Relatório de Gestão Financeira e Administrativa 2009 a 2015"
+    <Container title={data[0].title.rendered} className="flex flex-col">
+      <div
+        dangerouslySetInnerHTML={{ __html: updatedHTML }}
+        className="space-y-4"
       />
     </Container>
   );
