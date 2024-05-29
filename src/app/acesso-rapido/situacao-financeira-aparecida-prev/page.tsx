342dc8b85389ca5583b4b3603dd1f81d4b3e6af7
João Victor <jvbatistagomes11@gmail.com>
2024-05-29 16:40:31 -0300

    refactor: atualizado o html com o cheerio

--- src/app/acesso-rapido/situacao-financeira-aparecida-prev/page.tsx
+++ src/app/acesso-rapido/situacao-financeira-aparecida-prev/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
-import { extrairLinksDoHtml } from "@/utils/functions";
 import { AxiosInstance } from "@/services/axios";
 import { PostsProps } from "@/interfaces/interfaces";
 import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
 import Container from "@/components/container";
-import LinkAzul from "@/app/institucional/components/links";
 import Loading from "@/app/loading";
+import cheerio, { CheerioAPI } from "cheerio";
 
 export default function Home() {
   const [data, setData] = useState<PostsProps>();
@@ -31,58 +30,19 @@ export default function Home() {
   if (error) return <PaginaNaoEncontrada />;
   if (loading) return <Loading />;
 
-  const links = extrairLinksDoHtml(data?.content.rendered!);
-
-  const ano2024 = links
-    .map((link) => link.text)
-    .filter((link) => link.includes("2024"));
-  const ano2023 = links
-    .map((link) => link.text)
-    .filter((link) => link.includes("2023"));
-  const anosAnteriores = links
-    .map((link) => link.text)
-    .filter((link) => link.includes("Financeiro"));
+  const $: CheerioAPI = cheerio.load(data?.content.rendered!);
+  $("a").addClass("text-blue-500 hover:text-blue-700 hover:underline pl-5");
+  $("p").addClass("font-bold");
+  $("span").removeAttr("style");
+  $("strong").contents().unwrap();
+  const updateHtml = $.html();
 
   return (
-    <Container title="SITUAÇÃO FINANCEIRA DO APARECIDAPREV">
-      <Card title="ano de 2024">
-        <div className="flex flex-col">
-          {ano2024.map((mes) => (
-            <LinkAzul key={mes} href={mes} text={mes} />
-          ))}
-        </div>
-      </Card>
-      <Card title="ano de 2023">
-        <div className="flex flex-col">
-          {ano2023.map((mes) => (
-            <LinkAzul key={mes} href={mes} text={mes} />
-          ))}
-        </div>
-      </Card>
-      <Card title="anos anteriores">
-        <div className="flex flex-col">
-          {anosAnteriores.map((mes) => (
-            <LinkAzul key={mes} href={mes} text={mes} />
-          ))}
-        </div>
-      </Card>
+    <Container title={data?.title.rendered!}>
+      <div
+        dangerouslySetInnerHTML={{ __html: updateHtml }}
+        className="space-y-4"
+      />
     </Container>
   );
 }
-
-const Card = ({
-  title,
-  children,
-  className,
-}: {
-  title: string;
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <div className={`${className} border-b-[1px] py-3 dark:border-zinc-800`}>
-      <p className="text-lg font-semibold uppercase">{title}</p>
-      <div>{children}</div>
-    </div>
-  );
-};
