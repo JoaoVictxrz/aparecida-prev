bbc8eae55eafebf59737bb3b81f7360bb8a8d024
João Victor <jvbatistagomes11@gmail.com>
2024-06-05 15:58:34 -0300

    refactor: removido style para padronizar os titulos e adicionado target=_blank nos links

--- src/app/conselho/atas-do-conselho/page.tsx
+++ src/app/conselho/atas-do-conselho/page.tsx
@@ -35,11 +35,13 @@ export default function Home() {
   const $: CheerioAPI = cheerio.load(data.content.rendered);
   $("a").addClass("pl-5 text-blue-500 hover:text-blue-700 hover:underline");
   $("strong").removeAttr("style");
+  $("span").removeAttr("style");
   $("p").each(function () {
     if ($(this).html()?.trim() === "&nbsp;") {
       $(this).remove();
     }
   });
+  $("a").attr("target", "_blank");
   const updateHtml = $.html();
 
   return (
