7b5b4c159fd87f07928f38c51fd83fb4ded15c07
João Victor <jvbatistagomes11@gmail.com>
2024-06-06 14:50:21 -0300

    feat: adicionado titulo ao calendário de pagamentos

--- src/components/carrosel.tsx
+++ src/components/carrosel.tsx
@@ -26,25 +26,30 @@ export default function Carrosel() {
 
   return (
     <div className="w-full bg-white dark:bg-zinc-900">
-      <div className="group relative m-auto p-8 md:p-16">
-        <div className="flex items-center justify-center duration-500">
-          <Image
-            src="/banner-calendario.jpg"
-            alt="banner"
-            width={500}
-            height={500}
-            className="w-full max-w-7xl"
-          />
-        </div>
-        {/* Left Arrow */}
-        {/* <div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
+      <article className="flex w-full justify-center bg-white pb-20 dark:bg-zinc-900 dark:text-zinc-200">
+        <section className="container px-2">
+          <h1 className="my-8 text-lg font-bold uppercase md:text-2xl">
+            Calendário de pagamentos
+          </h1>
+          <div className="md: group relative m-auto">
+            <div className="flex items-center justify-center duration-500">
+              <Image
+                src="/banner-calendario.jpg"
+                alt="banner"
+                width={500}
+                height={500}
+                className="w-full max-w-7xl"
+              />
+            </div>
+            {/* Left Arrow */}
+            {/* <div className="absolute left-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div> */}
-        {/* Right Arrow */}
-        {/* <div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
+            {/* Right Arrow */}
+            {/* <div className="absolute right-5 top-[50%] hidden -translate-x-0 translate-y-[-50%] cursor-pointer rounded-full bg-black/20 p-2 text-2xl text-white group-hover:block">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div> */}
-        {/* <div className="top-4 flex justify-center py-2">
+            {/* <div className="top-4 flex justify-center py-2">
           {slides.map((slide, slideIndex) => (
             <div
               key={slideIndex}
@@ -55,7 +60,9 @@ export default function Carrosel() {
             </div>
           ))}
         </div> */}
-      </div>
+          </div>
+        </section>
+      </article>
     </div>
   );
 }
