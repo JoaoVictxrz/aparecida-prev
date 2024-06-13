743153326fc0ce4eb3e4b02f8fb518de3204bc79
João Victor <jvbatistagomes11@gmail.com>
2024-06-13 14:35:29 -0300

    feat: adicionado metadata para as paginas

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,11 +2,32 @@ import Container from "@/components/container";
 import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
 import { PostsProps } from "@/interfaces/interfaces";
 import { AxiosInstance } from "@/services/axios";
+import { getData } from "@/services/fetch";
 import cheerio, { CheerioAPI } from "cheerio";
+import { Metadata } from "next";
 
 interface Props {
   params: {
     slug: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const response = await AxiosInstance.get("/pages?slug=" + params.slug);
+
+  if (response.status !== 200) {
+    throw new Error("Erro ao buscar dados da página");
+  }
+
+  const contentRendered = response.data[0].content.rendered;
+
+  if (!contentRendered || contentRendered.trim().length === 0) {
+    throw new Error("Content is empty");
+  }
+
+  const data: PostsProps = response.data[0];
+  return {
+    title: data.title.rendered,
   };
 }
 
