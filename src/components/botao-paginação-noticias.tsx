814dbf31bca9f694cebb7ce45765844edfa0ad36
João Victor <jvbatistagomes11@gmail.com>
2024-07-16 15:19:02 -0300

    feat: adicionado disabled para desativar o botão enquanto a pagina estiver em loading

--- src/components/botao-paginação-noticias.tsx
+++ src/components/botao-paginação-noticias.tsx
@@ -2,15 +2,18 @@ export const Button = ({
   children,
   className,
   onClick,
+  disabled = false,
 }: {
   children: React.ReactNode;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }) => {
   return (
     <button
-      className={`rounded-sm border-2 px-2 dark:border-zinc-800 ${className}`}
+      className={`rounded-sm border-2 px-2 dark:border-zinc-800 ${className} ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
