2d40f8ea3e91d378fd8473aa33fd5c139386bcaf
João Victor <jvbatistagomes11@gmail.com>
2024-07-16 14:58:59 -0300

    refactor: refatoração do codigo para arrumar a paginação

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { extractTextFromHtml, formatarData } from "@/utils/functions";
 import { PostsProps, mediaProps } from "@/interfaces/interfaces";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { AxiosInstance } from "@/services/axios";
 import Container from "@/components/container";
 import Image from "next/image";
@@ -14,49 +14,63 @@ import {
 } from "react-icons/md";
 import Loading from "../loading";
 import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import { fetchMedia, fetchPosts } from "@/services/fetch-noticias";
+import { Button } from "@/components/botao-paginação-noticias";
+
+interface Props {
+  posts: PostsProps[];
+  media: mediaProps[];
+}
 
 export default function Home() {
+  const [proxPagina, setProxPagina] = useState<PostsProps[]>([]);
+  const [data, setData] = useState<Props | null>(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true);
-  const [posts, setPosts] = useState<PostsProps[]>([]);
   const [error, setError] = useState(false);
-  const [media, setMedia] = useState<mediaProps[]>([]);
-  const itensPorPagina = 10;
   const numeroMeio = currentPage;
   const proxNumero = numeroMeio + 1;
   const antNumero = numeroMeio - 1;
 
-  const fetchData = async (page: number) => {
+  const fetchData = useCallback(async (page: number) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     try {
-      const postsResponse = await AxiosInstance.get<PostsProps[]>(
-        `/posts?categories=2&page=${page}`,
-      );
-      setPosts(postsResponse.data);
-      const mediaIds = postsResponse.data.map(
-        (noticia) => noticia.featured_media,
-      );
-
-      const mediaResponses = [];
-
-      for (const mediaId of mediaIds) {
-        const response = await AxiosInstance.get<mediaProps[]>(
-          `/media?include=${mediaId}`,
-        );
-        mediaResponses.push(response.data);
-      }
-      setMedia(mediaResponses.map((response) => response[0]));
-      setLoading(false);
+      setLoading(true);
+      const posts = await fetchPosts(page);
+      const mediaIds = posts
+        .map((post) => post.featured_media)
+        .filter((id) => id);
+      const media = await fetchMedia(mediaIds);
+      const combinedData: Props = { posts, media };
+      setData(combinedData);
     } catch (error) {
-      console.error("Erro ao buscar posts:", error);
-      setLoading(false);
+      console.error("Erro ao buscar os dados:", error);
       setError(true);
+    } finally {
+      setLoading(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     fetchData(currentPage);
-  }, [currentPage]);
+  }, [currentPage, fetchData]);
+
+  useEffect(() => {
+    const loadNextPage = async () => {
+      try {
+        if (data && data.posts && data.posts.length >= 10) {
+          const nextPagePosts = await fetchPosts(currentPage + 1);
+          setProxPagina(nextPagePosts);
+        } else {
+          setProxPagina([]);
+        }
+      } catch (error) {
+        console.error("Erro ao buscar a próxima página:", error);
+        setProxPagina([]);
+      }
+    };
+    loadNextPage();
+  }, [currentPage, data]);
 
   if (loading) return <Loading />;
   if (error) return <PaginaNaoEncontrada />;
@@ -64,39 +78,46 @@ export default function Home() {
   return (
     <Container title="Notícias" className="flex flex-col items-center">
       <div className="max-w-5xl">
-        {posts?.map((post, i) => (
-          <div key={i}>
-            <div className="pb-10">
-              {media && media[i] && i < itensPorPagina && (
-                <Image
-                  src={media[i].guid.rendered}
-                  alt={media[i].source_url}
-                  width={media[i].media_details.width}
-                  height={media[i].media_details.height}
-                  className="h-96 w-auto"
+        {data?.posts.map((post, i) => {
+          const mediaItem = data.media.find(
+            (media) => media.id === post.featured_media,
+          );
+          return (
+            <div key={i}>
+              <div className="pb-10">
+                {mediaItem ? (
+                  <div className="h-full w-full">
+                    <Image
+                      src={mediaItem.source_url}
+                      alt={"teste"}
+                      width={mediaItem.media_details.width}
+                      height={200}
+                      className="object-cover"
+                    />
+                  </div>
+                ) : null}
+                <div
+                  className="text-2xl font-semibold"
+                  dangerouslySetInnerHTML={{ __html: post.title.rendered }}
                 />
-              )}
-              <div
-                className="text-2xl font-semibold"
-                dangerouslySetInnerHTML={{ __html: post.title.rendered }}
-              />
-              <p className="pb-4">Postado em: {formatarData(post.date)}</p>
-              <p className="pb-4">
-                {extractTextFromHtml(post.excerpt.rendered)}
-              </p>
+                <p className="pb-4">Postado em: {formatarData(post.date)}</p>
+                <p className="pb-4">
+                  {extractTextFromHtml(post.excerpt.rendered)}
+                </p>
 
-              <Link
-                href={`/noticias/${post.slug}`}
-                className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white transition-all hover:bg-blue-600 hover:underline"
-              >
-                Leia mais
-              </Link>
+                <Link
+                  href={`/noticias/${post.slug}`}
+                  className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white transition-all hover:bg-blue-600 hover:underline"
+                >
+                  Leia mais
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div className="flex w-full justify-center gap-[2px]">
-        {posts.length < 10 ? null : (
+        {data && data.posts.length < 10 ? null : (
           <>
             <Button
               onClick={() => (
@@ -129,7 +150,7 @@ export default function Home() {
               {numeroMeio}
             </Button>
             <Button
-              className={`${proxNumero > 13 && "hidden"}`}
+              className={`${proxPagina.length <= 0 && "hidden"}`}
               onClick={() => (
                 setCurrentPage(proxNumero),
                 window.scrollTo({ top: 0, behavior: "smooth" })
@@ -151,7 +172,7 @@ export default function Home() {
                 setCurrentPage(13),
                 window.scrollTo({ top: 0, behavior: "smooth" })
               )}
-              className={`${proxNumero > 13 && "hidden"}`}
+              className={`${proxPagina.length <= 0 && "hidden"}`}
             >
               <MdKeyboardDoubleArrowRight />
             </Button>
@@ -161,22 +182,3 @@ export default function Home() {
     </Container>
   );
 }
-
-const Button = ({
-  children,
-  className,
-  onClick,
-}: {
-  children: React.ReactNode;
-  className?: string;
-  onClick?: () => void;
-}) => {
-  return (
-    <button
-      className={`rounded-sm border-2 px-2 dark:border-zinc-800 ${className}`}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
