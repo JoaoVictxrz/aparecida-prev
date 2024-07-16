aeca039dda62485bf821ae5e2734970355b6d990
João Victor <jvbatistagomes11@gmail.com>
2024-07-16 15:18:32 -0300

    refactor: adicionado preload das paginas de paginação

--- src/app/noticias/page.tsx
+++ src/app/noticias/page.tsx
@@ -2,7 +2,6 @@
 import { extractTextFromHtml, formatarData } from "@/utils/functions";
 import { PostsProps, mediaProps } from "@/interfaces/interfaces";
 import { useCallback, useEffect, useState } from "react";
-import { AxiosInstance } from "@/services/axios";
 import Container from "@/components/container";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,7 +9,6 @@ import {
   MdKeyboardArrowLeft,
   MdKeyboardArrowRight,
   MdKeyboardDoubleArrowLeft,
-  MdKeyboardDoubleArrowRight,
 } from "react-icons/md";
 import Loading from "../loading";
 import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
@@ -28,6 +26,7 @@ export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [hasMorePages, setHasMorePages] = useState(true);
   const numeroMeio = currentPage;
   const proxNumero = numeroMeio + 1;
   const antNumero = numeroMeio - 1;
@@ -36,6 +35,7 @@ export default function Home() {
     window.scrollTo({ top: 0, behavior: "smooth" });
     try {
       setLoading(true);
+      setError(false);
       const posts = await fetchPosts(page);
       const mediaIds = posts
         .map((post) => post.featured_media)
@@ -43,6 +43,13 @@ export default function Home() {
       const media = await fetchMedia(mediaIds);
       const combinedData: Props = { posts, media };
       setData(combinedData);
+      setHasMorePages(posts.length === 10);
+      if (posts.length === 10) {
+        const nextPagePosts = await fetchPosts(page + 1);
+        setProxPagina(nextPagePosts);
+      } else {
+        setProxPagina([]);
+      }
     } catch (error) {
       console.error("Erro ao buscar os dados:", error);
       setError(true);
@@ -55,22 +62,21 @@ export default function Home() {
     fetchData(currentPage);
   }, [currentPage, fetchData]);
 
-  useEffect(() => {
-    const loadNextPage = async () => {
-      try {
-        if (data && data.posts && data.posts.length >= 10) {
-          const nextPagePosts = await fetchPosts(currentPage + 1);
-          setProxPagina(nextPagePosts);
-        } else {
-          setProxPagina([]);
-        }
-      } catch (error) {
-        console.error("Erro ao buscar a próxima página:", error);
-        setProxPagina([]);
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    if (proxPagina.length > 0) {
+      setData({ posts: proxPagina, media: data?.media || [] });
+      setProxPagina([]);
+      setHasMorePages(proxPagina.length === 10);
+      if (proxPagina.length === 10) {
+        fetchPosts(page + 1).then((nextPagePosts) =>
+          setProxPagina(nextPagePosts),
+        );
       }
-    };
-    loadNextPage();
-  }, [currentPage, data]);
+    } else {
+      fetchData(page);
+    }
+  };
 
   if (loading) return <Loading />;
   if (error) return <PaginaNaoEncontrada />;
@@ -120,61 +126,42 @@ export default function Home() {
         {data && data.posts.length < 10 ? null : (
           <>
             <Button
-              onClick={() => (
-                setCurrentPage(1),
-                window.scrollTo({ top: 0, behavior: "smooth" })
-              )}
-              className={`${antNumero < 1 && "hidden"}`}
+              onClick={() => handlePageChange(1)}
+              className={`${currentPage === 1 ? "hidden" : ""}`}
+              disabled={loading}
             >
               <MdKeyboardDoubleArrowLeft />
             </Button>
             <Button
-              className={`${antNumero < 1 && "hidden"}`}
-              onClick={() => (
-                setCurrentPage(currentPage - 1),
-                window.scrollTo({ top: 0, behavior: "smooth" })
-              )}
+              onClick={() => handlePageChange(currentPage - 1)}
+              className={`${currentPage === 1 ? "hidden" : ""}`}
+              disabled={loading}
             >
               <MdKeyboardArrowLeft />
             </Button>
             <Button
-              className={`${antNumero < 1 && "hidden"}`}
-              onClick={() => (
-                setCurrentPage(antNumero),
-                window.scrollTo({ top: 0, behavior: "smooth" })
-              )}
+              onClick={() => handlePageChange(antNumero)}
+              className={`${currentPage === 1 ? "hidden" : ""}`}
+              disabled={loading}
             >
               {antNumero}
             </Button>
-            <Button className="bg-zinc-200 dark:bg-zinc-800">
-              {numeroMeio}
+            <Button className="bg-zinc-200 dark:bg-zinc-800" disabled>
+              {currentPage}
             </Button>
             <Button
-              className={`${proxPagina.length <= 0 && "hidden"}`}
-              onClick={() => (
-                setCurrentPage(proxNumero),
-                window.scrollTo({ top: 0, behavior: "smooth" })
-              )}
+              onClick={() => handlePageChange(proxNumero)}
+              className={`${!hasMorePages ? "hidden" : ""}`}
+              disabled={loading}
             >
               {proxNumero}
             </Button>
             <Button
-              className={`${proxNumero > 13 && "hidden"}`}
-              onClick={() => (
-                setCurrentPage(currentPage + 1),
-                window.scrollTo({ top: 0, behavior: "smooth" })
-              )}
+              onClick={() => handlePageChange(currentPage + 1)}
+              className={`${!hasMorePages ? "hidden" : ""}`}
+              disabled={loading}
             >
               <MdKeyboardArrowRight />
-            </Button>
-            <Button
-              onClick={() => (
-                setCurrentPage(13),
-                window.scrollTo({ top: 0, behavior: "smooth" })
-              )}
-              className={`${proxPagina.length <= 0 && "hidden"}`}
-            >
-              <MdKeyboardDoubleArrowRight />
             </Button>
           </>
         )}
