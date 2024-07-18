eec1489f0f1204e4dd0c501a9408f42108c09bc2
João Victor <jvbatistagomes11@gmail.com>
2024-07-18 14:41:17 -0300

    feat: adicionado ir para o topo da tela na pagina inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,13 @@ import Transparencia from "@/components/transparencia";
 import AcessoRapido from "@/components/acesso-rapido";
 import Noticias from "@/components/noticias";
 import Carrosel from "@/components/carrosel";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
   return (
     <main className="flex min-h-screen w-full flex-col items-center text-black">
       <BackgroundImage />
