6036efca7f87d6aafd7d76af15da1cb7880f68d1
João Victor <jvbatistagomes11@gmail.com>
2024-08-06 16:31:34 -0300

    feat: adicionado função para formatar texto do titulo

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,3 +1,5 @@
+import { extractTextFromHtml } from "@/utils/functions";
+
 export default function Title({
   title,
   postadoEm,
@@ -7,7 +9,9 @@ export default function Title({
 }) {
   return (
     <div className="my-8 border-b-[1px] pb-2 dark:border-zinc-700 dark:text-zinc-100">
-      <h1 className="text-lg font-semibold uppercase  md:text-2xl">{title}</h1>
+      <h1 className="text-lg font-semibold uppercase  md:text-2xl">
+        {extractTextFromHtml(title)}
+      </h1>
       {postadoEm && (
         <p className="text-base font-extralight">Postado em: {postadoEm}</p>
       )}
