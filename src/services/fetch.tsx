7ce59ca8505d5026897c3b5a1536b683927f57af
João Victor <jvbatistagomes11@gmail.com>
2024-08-06 16:31:02 -0300

    feat: adicionado status code no get da api e refatoração

--- src/services/fetch.tsx
+++ src/services/fetch.tsx
@@ -1,57 +1,69 @@
 import { mediaProps, PostsProps } from "@/interfaces/interfaces";
 import { AxiosInstance } from "./axios";
+import { AxiosError } from "axios";
+
+type StatusCode = {
+  404: "Bad Request";
+  200: "OK";
+  204: "No Content";
+  500: "Internal Server Error";
+};
+
+interface Response<T> {
+  statusCode: keyof StatusCode;
+  data: T;
+}
 
 export class getAllDataPages {
   constructor() {}
 
-  async getAllPages(url: string): Promise<PostsProps[]> {
+  private async getData<T>(url: string): Promise<Response<T[]>> {
     try {
-      const response = await AxiosInstance.get(`/pages${url}`);
-      if (response.data.length < 0) {
-        throw new Error("A página não existe ou está vázia");
+      const response = await AxiosInstance.get(url);
+      if (response.data.length === 0) {
+        return {
+          statusCode: 204,
+          data: [] as T[],
+        };
       }
       if (response.data.length > 10) {
-        const responsePage2 = await AxiosInstance.get(`/pages${url}&page=2`);
+        const responsePage2 = await AxiosInstance.get(`${url}&page=2`);
         response.data.push(...responsePage2.data);
       }
-      const data = response.data;
-      return data;
+      return { statusCode: 200, data: response.data };
     } catch (error) {
+      if (error instanceof AxiosError) {
+        if (error.response && error.response.status === 404) {
+          return {
+            statusCode: 404,
+            data: [],
+          };
+        }
+      } else if (error instanceof Error) {
+        throw new Error(error.message);
+      }
       throw error;
     }
   }
 
-  async getAllPosts(url: string): Promise<PostsProps[]> {
-    try {
-      const response = await AxiosInstance.get(`/posts${url}`);
-      if (response.data.length < 0) {
-        throw new Error("A página não existe ou está vázia");
-      }
-      if (response.data.length > 10) {
-        const responsePage2 = await AxiosInstance.get(`/posts${url}&page=2`);
-        response.data.push(...responsePage2.data);
-      }
-      const data = response.data;
-      return data;
-    } catch (error) {
-      throw error;
-    }
+  async getAllPages(url: string): Promise<Response<PostsProps[]>> {
+    const response = await this.getData<PostsProps>(`/pages${url}`);
+    return { statusCode: response.statusCode, data: response.data };
   }
 
-  async getAllMedia(url: string): Promise<mediaProps[]> {
-    try {
-      const response = await AxiosInstance.get(`/media${url}`);
-      const data = response.data;
-      if (response.data.length < 0) {
-        throw new Error("A página não existe ou está vázia");
-      }
-      if (data.length > 10) {
-        const responsePage2 = await AxiosInstance.get(`/media${url}&page=2`);
-        data.push(...responsePage2.data);
-      }
-      return data;
-    } catch (error) {
-      throw error;
+  async getAllPosts(url: string): Promise<Response<PostsProps[]>> {
+    const response = await this.getData<PostsProps>(`/posts${url}`);
+    if (response.statusCode === 204) {
+      return {
+        statusCode: 204,
+        data: [] as PostsProps[],
+      };
     }
+    return { statusCode: response.statusCode, data: response.data };
+  }
+
+  async getAllMedia(url: string): Promise<Response<mediaProps[]>> {
+    const response = await this.getData<mediaProps>(`/media${url}`);
+    return { statusCode: response.statusCode, data: response.data };
   }
 }
