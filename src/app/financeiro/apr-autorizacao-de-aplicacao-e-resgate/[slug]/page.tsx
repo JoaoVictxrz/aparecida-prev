06c9bdb161dd77348bab9a7820b0dc71d3e15843
João Victor <jvbatistagomes11@gmail.com>
2024-08-08 13:35:01 -0300

    refactor: Alterado o get da api e adicionado novo componente para as paginas slug

--- src/app/financeiro/apr-autorizacao-de-aplicacao-e-resgate/[slug]/page.tsx
+++ src/app/financeiro/apr-autorizacao-de-aplicacao-e-resgate/[slug]/page.tsx
@@ -1,12 +1,6 @@
 "use client";
-import { useEffect, useState } from "react";
-import { AxiosInstance } from "@/services/axios";
-import { PostsProps } from "@/interfaces/interfaces";
-import cheerio, { CheerioAPI } from "cheerio";
-import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
-import Container from "@/components/container";
-import Loading from "@/app/loading";
-import { extractTextFromHtml } from "@/utils/functions";
+import PaginaSlug from "@/components/slug-page";
+import useFetchPosts from "@/hooks/useFetchPosts";
 
 interface Props {
   params: {
@@ -15,39 +9,7 @@ interface Props {
 }
 
 export default function Page({ params }: Props) {
-  const [posts, setPosts] = useState<PostsProps[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const { posts, loading, error } = useFetchPosts("?slug=" + params.slug);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await AxiosInstance.get("/posts?slug=" + params.slug);
-        setPosts(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.log("Erro ao buscar dados da página:", error);
-        setLoading(false);
-        setError(true);
-      }
-    };
-    fetchData();
-  }, [params.slug]);
-
-  if (loading) return <Loading />;
-  if (error) return <PaginaNaoEncontrada />;
-
-  const $: CheerioAPI = cheerio.load(posts[0]?.content.rendered);
-  $("a").addClass("pl-5 text-blue-500 hover:text-blue-700 hover:underline");
-  $("a").attr("target", "_blank");
-  const updatedHTML = $.html();
-
-  return (
-    <Container title={extractTextFromHtml(posts[0]?.title.rendered)}>
-      <div
-        dangerouslySetInnerHTML={{ __html: updatedHTML }}
-        className="space-y-2"
-      />
-    </Container>
-  );
+  return <PaginaSlug data={posts} loading={loading} error={error} />;
 }
--- src/app/financeiro/calculo-atuarial/[slug]/page.tsx
+++ src/app/financeiro/calculo-atuarial/[slug]/page.tsx
@@ -1,12 +1,6 @@
 "use client";
-import { extractTextFromHtml, extrairLinksDoHtml } from "@/utils/functions";
-import { useEffect, useState } from "react";
-import { AxiosInstance } from "@/services/axios";
-import { PostsProps } from "@/interfaces/interfaces";
-import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
-import Container from "@/components/container";
-import Loading from "@/app/loading";
-import LinkAzul from "@/app/institucional/components/links";
+import PaginaSlug from "@/components/slug-page";
+import useFetchPosts from "@/hooks/useFetchPosts";
 
 interface Props {
   params: {
@@ -15,44 +9,7 @@ interface Props {
 }
 
 export default function Page({ params }: Props) {
-  const [posts, setPosts] = useState<PostsProps[]>();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const { posts, loading, error } = useFetchPosts("?slug=" + params.slug);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await AxiosInstance.get("/posts?slug=" + params.slug);
-        setPosts(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.log("Erro ao buscar dados da página:", error);
-        setLoading(false);
-        setLoading(true);
-      }
-    };
-    fetchData();
-  }, [params.slug]);
-
-  if (loading) return <Loading />;
-  if (error) return <PaginaNaoEncontrada />;
-
-  return (
-    <div>
-      {posts?.map((post) => {
-        const links = extrairLinksDoHtml(post.content.rendered);
-        return (
-          <Container
-            title={extractTextFromHtml(post.title.rendered)}
-            key={post.id}
-            className="grid max-w-xl"
-          >
-            {links.map((link, i) => (
-              <LinkAzul key={i} href={link.url} text={link.text} />
-            ))}
-          </Container>
-        );
-      })}
-    </div>
-  );
+  return <PaginaSlug data={posts} loading={loading} error={error} />;
 }
