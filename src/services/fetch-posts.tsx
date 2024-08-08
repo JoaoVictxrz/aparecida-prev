9b1f5a6c0d7e463c35e029ab50608e6d970c7777
João Victor <jvbatistagomes11@gmail.com>
2024-08-08 15:59:05 -0300

    refactor: alterado logica para melhor utilização do fetch

--- src/services/fetch-posts.tsx
+++ src/services/fetch-posts.tsx
@@ -1,9 +1,9 @@
 import { getAllDataPages } from "@/services/fetch";
 import { PostsProps } from "@/interfaces/interfaces";
 
-export const fetchPostsBySlug = async (slug: string): Promise<PostsProps[]> => {
+export const fetchPosts = async (slug: string): Promise<PostsProps[]> => {
   const getData = new getAllDataPages();
-  const response = await getData.getAllPosts(`?slug=${slug}`);
+  const response = await getData.getAllPosts(`${slug}`);
   if (response.statusCode === 204) {
     throw new Error("Nenhuma postagem encontrada.");
   }
