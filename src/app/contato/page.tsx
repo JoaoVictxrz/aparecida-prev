dfc9792cfb259e15dbf65271710efdefe81ebe6b
João Victor <jvbatistagomes11@gmail.com>
2024-08-13 15:07:43 -0300

    feat: adicionado integração com api e refatorado codigo

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,27 +1,23 @@
+"use client";
 import Container from "@/components/container";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Contato",
-};
+import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import useFetchPages from "@/hooks/useFetchPages";
+import Loading from "../loading";
 
 export default function Contato() {
+  const { pages, error, loading } = useFetchPages("?slug=contato");
+
+  if (error) return <PaginaNaoEncontrada />;
+  if (loading) return <Loading />;
+  if (!pages) return;
+  const page = pages[0];
+
   return (
-    <Container title="Contato">
-      <p className="text-lg font-bold">Endereço AparecidaPrev</p>
-      <p>
-        Avenida Santana Quadra 1 Lotes 5 e 6, Setor Célia Maria, CEP: 74987-828
-        Aparecida de Goiânia – Goiás
-      </p>
-      <br />
-      <p className="text-lg font-bold">Telefone geral:</p>
-      <p>(62) 3545-5934</p>
-      <br />
-      <p className="text-lg font-bold">Telefone perícia:</p>
-      <p>(62) 3983-7052 (whatsapp)</p>
-      <br />
-      <p className="text-lg font-bold">E-mail Diretoria de benefícios:</p>
-      <p>beneficios@aparecidaprev.go.gov.br</p>
+    <Container title={page.title.rendered}>
+      <div
+        dangerouslySetInnerHTML={{ __html: page.content.rendered }}
+        className="space-y-4"
+      />
     </Container>
   );
 }
