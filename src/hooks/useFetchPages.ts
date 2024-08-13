a83706fe3b30cfb7bfeade6f2a6b4a1f5ed02727
João Victor <jvbatistagomes11@gmail.com>
2024-08-13 16:39:26 -0300

    refactor: alterado logica para ficar mais limpo e legivel

--- src/hooks/useFetchPages.ts
+++ src/hooks/useFetchPages.ts
@@ -3,27 +3,31 @@ import { useState, useEffect } from "react";
 import { PostsProps } from "@/interfaces/interfaces";
 import { fetchPages } from "@/services/fetch-api";
 
+interface Props {
+  pages: PostsProps[] | null;
+  loading: boolean;
+  error: boolean;
+}
+
 export default function useFetchPages(slug: string) {
-  const [pages, setPages] = useState<PostsProps[] | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [state, setState] = useState<Props>({
+    pages: null,
+    loading: true,
+    error: false,
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await fetchPages(slug);
-        setPages(data);
+        setState({ pages: data, loading: false, error: false });
       } catch (err) {
         console.log("Erro ao buscar dados da página:", err);
-        if (err instanceof Error) {
-          setError(true);
-        }
-      } finally {
-        setLoading(false);
+        setState({ pages: null, loading: false, error: true });
       }
     };
     fetchData();
   }, [slug]);
 
-  return { pages, loading, error };
+  return state;
 }
