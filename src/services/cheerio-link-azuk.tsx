9242a843b0c75968341db499a0c0113e9da980e3
João Victor <jvbatistagomes11@gmail.com>
2024-08-15 15:45:43 -0300

    refactor: removido o strong unwarp

--- src/services/cheerio-link-azuk.tsx
+++ src/services/cheerio-link-azuk.tsx
@@ -2,7 +2,6 @@ import * as cheerio from "cheerio";
 export function CheerioLink(data: string) {
   const $ = cheerio.load(data);
   $("a").addClass("pl-5 text-blue-500 hover:underline hover:text-blue-700");
-  $("strong").contents().unwrap();
   const span = $("span");
   if (span) {
     span.removeAttr("style");
