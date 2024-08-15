f3ae8d4e98a8d6f86aaae4da3ed8c898105e7712
João Victor <jvbatistagomes11@gmail.com>
2024-08-15 15:53:45 -0300

    refactor: alterado fetch agora usando o hook fetchPages

--- src/app/institucional/taxa-de-administracao/page.tsx
+++ src/app/institucional/taxa-de-administracao/page.tsx
@@ -1,53 +1,23 @@
 "use client";
-import { useEffect, useState } from "react";
-import { extrairLinksDoHtml } from "@/utils/functions";
-import { AxiosInstance } from "@/services/axios";
-import { PostsProps } from "@/interfaces/interfaces";
+import { CheerioLink } from "@/services/cheerio-link-azuk";
 import PaginaNaoEncontrada from "@/components/pagina-nao-encontrada";
+import useFetchPages from "@/hooks/useFetchPages";
 import Container from "@/components/container";
-import LinkAzul from "../components/links";
 import Loading from "@/app/loading";
 
 export default function Home() {
-  const [data, setData] = useState<PostsProps>();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const fetchData = async () => {
-    try {
-      const response = await AxiosInstance.get("/pages/1820");
-      setData(response.data);
-      setLoading(false);
-    } catch (error) {
-      console.log("erro ao buscar dados: " + error);
-      setLoading(false);
-      setError(true);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+  const { error, loading, pages } = useFetchPages(
+    "?slug=taxa-de-administracao",
+  );
   if (error) return <PaginaNaoEncontrada />;
   if (loading) return <Loading />;
 
-  const links = extrairLinksDoHtml(data?.content.rendered!);
+  const data = pages![0];
 
+  const updateHtml = CheerioLink(data.content.rendered!);
   return (
     <Container title={data?.title.rendered!}>
-      <div className="flex flex-col">
-        <p className="font-semibold uppercase">
-          PORTARIA INSTITUI A TAXA ADMINISTRATIVA
-        </p>
-        <div className="flex flex-col gap-1 pl-5 pt-2">
-          {links.map((link, i) => (
-            <div key={i}>
-              <LinkAzul text={link.text} href={link.url} />
-            </div>
-          ))}
-        </div>
-      </div>
+      <div dangerouslySetInnerHTML={{ __html: updateHtml }} />
     </Container>
   );
 }
