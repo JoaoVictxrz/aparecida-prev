89a6a84a4aeb4d2f52465fafef0c1cc5e06dac90
João Victor <jvbatistagomes11@gmail.com>
2024-08-22 16:12:08 -0300

    feat: adicionado loop no fetch da api para facilitar a paginação

--- src/hooks/useNotices.tsx
+++ src/hooks/useNotices.tsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import useFetchPages from "./useFetchPages";
-import Loading from "@/app/loading";
 import { mediaProps, PostsProps } from "@/interfaces/interfaces";
 import { fetchMedia, fetchPosts } from "@/services/fetch-api";
 
@@ -24,17 +22,50 @@ export function useNotices(url: string) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const posts = await fetchPosts(url);
+        const posts = await fetchPosts(url + "&per_page=100");
         const media = await fetchMedia(`/${posts[0].featured_media}`);
-
         setState({
           notices: { media: media, post: posts },
           loading: false,
           error: false,
         });
+
+        if (posts.length >= 100) {
+          const allPosts = [...posts];
+          const allMedia = [...media];
+          for (let i = 2; i <= Math.ceil(posts.length / 100); i++) {
+            const postsResponse = await fetchPosts(
+              url + `&per_page=100&page=${i}`,
+            );
+            if (postsResponse.length === 0) break;
+            if (postsResponse.length > 0) {
+              allPosts.push(...postsResponse);
+              if (postsResponse.length > 0) {
+                for (const posts of postsResponse) {
+                  const mediaResponse = await fetchMedia(
+                    `/${posts.featured_media}`,
+                  );
+                  allMedia.push(...mediaResponse);
+                }
+              }
+            }
+            setState({
+              notices: {
+                media: allMedia,
+                post: allPosts,
+              },
+              loading: false,
+              error: false,
+            });
+          }
+        }
       } catch (error) {
         console.log("Erro ao buscar dados da página:", error);
-        setState({ notices: null, loading: false, error: true });
+        setState({
+          notices: null,
+          loading: false,
+          error: true,
+        });
       }
     };
 
